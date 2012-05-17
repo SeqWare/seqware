b862d5d6e0ada55aedb51c809cf5eb1f5f108873
Joachim Baran <Joachim.Baran@oicr.on.ca>
2012-05-17 16:37:41 -0400

    Added predecessor relationship to Versionable objects.
    Unsatisfactory, because the type of the current and preceding
    instances is hidden by the interface. Might be a good idea to
    use Feature here or a generic type that can be re-used by
    FeatureSets too.

--- seqware-queryengine/src/main/java/com/github/seqware/model/Versionable.java
+++ seqware-queryengine/src/main/java/com/github/seqware/model/Versionable.java
@@ -8,13 +8,27 @@ public interface Versionable {
     
     /**
      * Get version for the subject
-     * @return 
+     * @return Version of the subject.
      */
     public String getVersion();
     
     /**
      * Set version for a subject
-     * @param String version to set on subject
+     * @param version Version to set on subject.
      */
     public void setVersion(String version);
+
+    /**
+     * Returns the subject (if any) that represents a previous version.
+     *
+     * @return Versionable Previous (preceding) subject that represents an earlier version of the subject.
+     */
+    public Versionable getPrecedingVersion();
+
+    /**
+     * Sets the relationship to an earlier version of the subject.
+     *
+     * @param predecessor Preceding subject that represents an earlier version of this subject.
+     */
+    public void setPrecedingVersion(Versionable predecessor);
 }
