b41b13858f54e7970095ff18af2b5d0bdca2563e
Joachim Baran <Joachim.Baran@oicr.on.ca>
2012-06-06 17:09:48 -0400

    Added test for SimplePersistentBackEnd.
    Throws exception because it cannot find its .bin file. Occured when
    the serialization code was moved out of the actual test method.

---
+++ seqware-queryengine/src/test/java/com/github/seqware/impl/test/SimplePersistentBackEndTest.java
@@ -0,0 +1,46 @@
+package com.github.seqware.impl.test;
+
+import com.github.seqware.factory.Factory;
+import com.github.seqware.factory.ModelManager;
+import com.github.seqware.impl.ApacheUtilsPersistentSerialization;
+import com.github.seqware.impl.SimplePersistentBackEnd;
+import com.github.seqware.model.Feature;
+import com.github.seqware.model.FeatureSet;
+import com.github.seqware.model.Particle;
+import com.github.seqware.model.test.FeatureStoreInterfaceTest;
+import junit.framework.Assert;
+import org.junit.Test;
+
+/**
+ * Tests for the {@link SimplePersistentBackEnd}.
+ *
+ * @author jbaran
+ */
+public class SimplePersistentBackEndTest extends FeatureStoreInterfaceTest {
+    @Test
+    public void storageAndRetrievalTest() {
+        SimplePersistentBackEnd backend = new SimplePersistentBackEnd(new ApacheUtilsPersistentSerialization());
+
+        try {
+            backend.store(this.aSet);
+        }
+        catch(Exception e) {
+            Assert.assertTrue("Backend could not store the given FeatureSet.", false);
+        }
+
+        Particle particle = null;
+        try {
+            backend.getParticleByUUID(this.aSet.getUUID());
+        }
+        catch(Exception e) {
+            Assert.assertTrue("Could not retrieve the previously stored FeatureSet.", false);
+        }
+
+        if (!(particle instanceof FeatureSet))
+            Assert.assertTrue("Returned result does not match the previously stored object by type.", false);
+
+        FeatureSet returnedSet = (FeatureSet)particle;
+
+        Assert.assertTrue("Returned feature set does not contain the same amount of features as were stored.", this.aSet.getCount() == returnedSet.getCount());
+    }
+}
--- seqware-queryengine/src/test/java/com/github/seqware/model/test/FeatureStoreInterfaceTest.java
+++ seqware-queryengine/src/test/java/com/github/seqware/model/test/FeatureStoreInterfaceTest.java
@@ -11,22 +11,28 @@ import org.junit.Test;
  * Unit tests of BackEndInterface.
  *
  * @author dyuen
+ * @author jbaran
  */
 public class FeatureStoreInterfaceTest {
 
+    protected static FeatureSet aSet;
+    protected static Feature a1, a2, a3;
+
+    static {
+        ModelManager mManager = Factory.getModelManager();
+        aSet = mManager.buildFeatureSet().setReference(mManager.buildReference().setName("Dummy ref").build()).build();
+        // create and store some features
+        a1 = mManager.buildFeature().setStart(1000000).setStop(1000100).build();
+        a2 = mManager.buildFeature().setStart(1000200).setStop(1000300).build();
+        a3 = mManager.buildFeature().setStart(1000400).setStop(1000500).build();
+        aSet.add(a1);
+        aSet.add(a2);
+        aSet.add(a3);
+        mManager.flush();
+    }
 
     @Test
     public void testFeatureCreationAndIterate() {
-        ModelManager mManager = Factory.getModelManager();
-        FeatureSet aSet = mManager.buildFeatureSet().setReference(mManager.buildReference().setName("Dummy ref").build()).build();
-        // create and store some features
-        Feature a1 = mManager.buildFeature().setStart(1000000).setStop(1000100).build();
-        Feature a2 = mManager.buildFeature().setStart(1000200).setStop(1000300).build();
-        Feature a3 = mManager.buildFeature().setStart(1000400).setStop(1000500).build();
-        aSet.add(a1);         
-        aSet.add(a2);         
-        aSet.add(a3);
-        mManager.flush();
         // get FeatureSets from the back-end
         boolean b1 = false;
         boolean b2 = false;
