8b24587a86b2b71d62bda5d6b61cb2238e796230
Denis Yuen <dyuen@oicr.on.ca>
2012-07-05 18:48:44 -0400

    laggard

--- seqware-queryengine/src/main/java/com/github/seqware/queryengine/factory/package-info.java
+++ seqware-queryengine/src/main/java/com/github/seqware/queryengine/factory/package-info.java
@@ -2,4 +2,4 @@
  * Provides the classes that the restful interface will use to construct
  * interfaces to the back-end without reference to the specific back-end
  */
-package com.github.seqware.factory;
+package com.github.seqware.queryengine.factory;
