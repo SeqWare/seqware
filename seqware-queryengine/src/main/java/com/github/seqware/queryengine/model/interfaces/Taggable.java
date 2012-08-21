9e90de0c586c70e8561fa395b45efe3a333b00ef
Denis Yuen <dyuen@oicr.on.ca>
2012-08-21 19:48:28 -0400

    Initial implemention of nested hash structure for DCC.
    Will need to think about how to make this nicer and/or cleaner.
    
    
    Former-commit-id: aadb3aea2d7ccc6f900d8da4960ecdf94393e642

--- seqware-queryengine/src/main/java/com/github/seqware/queryengine/model/interfaces/Taggable.java
+++ seqware-queryengine/src/main/java/com/github/seqware/queryengine/model/interfaces/Taggable.java
@@ -2,6 +2,10 @@ package com.github.seqware.queryengine.model.interfaces;
 
 import com.github.seqware.queryengine.model.Tag;
 import com.github.seqware.queryengine.util.SeqWareIterable;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
 
 /**
  * Interface for all classes that can be tagged. Associating or dissociating 
@@ -31,10 +35,35 @@ public interface Taggable<T> {
     public SeqWareIterable<Tag> getTags();
     
     /**
+     * Get tags that have been sorted into a structure of Nested Maps.
+     * Splits are handled by '::'
+     * i.e. MTTS::dbSNP::ID becomes MTTS -> dbSNP -> ID
+     * @return 
+     */
+    public NestedLevel getNestedTags();
+    
+    /**
      * Quickly get the value of a tag by the key.
      * @param key
      * @return 
      */
     public Tag getTagByKey(String key);
     
+    
+    /**
+     * This class represents both the tags that may be present at a particular level of the nested hash structure
+     * as well as tags that may be further up
+     */
+    public class NestedLevel{
+        protected Map<String, NestedLevel> childMaps = new HashMap<String, NestedLevel>();
+        protected Map<String, Tag> childTags= new HashMap<String, Tag>();
+
+        public Map<String, NestedLevel> getChildMaps() {
+            return childMaps;
+        }
+
+        public Map<String, Tag> getChildTags() {
+            return childTags;
+        }
+    }
 }
