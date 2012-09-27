37f49d700aefd5a40c676015b125a9a774769812
yliang9 <yliang9@gmail.com>
2012-09-27 13:08:15 -0400

    add abstractworkflowengine
    Former-commit-id: 149a3a735df4539346b12a23900f44698348d2bc

---
+++ seqware-pipeline/src/main/java/net/sourceforge/seqware/pipeline/workflowV2/pegasus/PegasusWorkflowEngine.java
@@ -0,0 +1,7 @@
+package net.sourceforge.seqware.pipeline.workflowV2.pegasus;
+
+import net.sourceforge.seqware.pipeline.workflowV2.AbstractWorkflowEngine;
+
+public class PegasusWorkflowEngine extends AbstractWorkflowEngine {
+
+}
