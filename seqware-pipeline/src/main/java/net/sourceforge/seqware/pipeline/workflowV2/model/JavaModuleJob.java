dabd355467d13c2a957c02c0b07c7603056ac6df
yliang9 <yliang9@gmail.com>
2012-10-08 23:08:50 -0400

    continue java workflow

--- seqware-pipeline/src/main/java/net/sourceforge/seqware/pipeline/workflowV2/model/JavaModuleJob.java
+++ seqware-pipeline/src/main/java/net/sourceforge/seqware/pipeline/workflowV2/model/JavaModuleJob.java
@@ -2,8 +2,8 @@ package net.sourceforge.seqware.pipeline.workflowV2.model;
 
 public class JavaModuleJob extends AbstractJob {
 
-	public JavaModuleJob(String algo) {
-		super(algo);
+	public JavaModuleJob(String algo, String classpath, String module) {
+		super(algo, classpath, module);
 	}
 
 }
