5a1c1e0bedb1d2d9b6737d6c3171ea0b552cf1bb
yliang9 <yliang9@gmail.com>
2012-10-11 17:05:26 -0400

    rm file
    Former-commit-id: 51ef51fa2d8e26f91c1a24fcdad7d5d118369350

--- seqware-pipeline/src/main/java/net/sourceforge/seqware/pipeline/workflowV2/model/JavaModuleJob.java
+++
@@ -1,9 +0,0 @@
-package net.sourceforge.seqware.pipeline.workflowV2.model;
-
-public class JavaModuleJob extends AbstractJob {
-
-	public JavaModuleJob(String algo, String classpath, String module) {
-		super(algo, classpath, module);
-	}
-
-}
