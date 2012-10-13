dd9a9cc86aac971c4d0041d8277f6a8db69f9615
Brian O'Connor <briandoconnor@gmail.com>
2012-10-13 01:17:03 -0400

    I think the JUnit accordian view is working now. The invoices have collapsible sections for each expense type and I also revised the rounding in the application so currencies print with two decimal places. It would still be better to move everything to BigDecimal. I also could make the UI more consistent with the site and make line items a single line rather than split across lines inside each section (to save space).
    Former-commit-id: 9e9c76cc6927d2609e05ecf75769d990fb396f37

--- seqware-common/src/main/java/net/sourceforge/seqware/common/model/Expense.java
+++ seqware-common/src/main/java/net/sourceforge/seqware/common/model/Expense.java
@@ -1,6 +1,7 @@
 package net.sourceforge.seqware.common.model;
 
 import java.io.Serializable;
+import java.math.BigDecimal;
 import java.util.Date;
 import java.util.Set;
 import java.util.SortedSet;
@@ -21,90 +22,106 @@ import org.apache.log4j.Logger;
  * @version $Id: $Id
  */
 public class Expense implements Serializable, Comparable<Expense>, PermissionsAware {
-  
-  private static final long serialVersionUID = 1L;
-  private Integer expenseId;
-  private Invoice invoice;
-  private WorkflowRun workflowRun;
-  private String agent;
-  private String expenseType;
-  private String description;
-  private Double pricePerUnit;
-  private Double totalUnits;
-  private Double totalPrice;
-  private Double addedSurcharge;
-  private Integer swAccession;
-  private Date finishedTimestamp;
-  private Logger logger;
-  //private Set<ExpenseAttribute> invoiceAttributes = new TreeSet<ExpenseAttribute>();
 
-  /**
-   * <p>Constructor for Expense.</p>
-   */
-  public Expense() {
-    super();
-    logger = Logger.getLogger(Expense.class);
-  }
-
-  /** {@inheritDoc} */
-  @Override
-  public int compareTo(Expense that) {
-    if (that == null)
-      return -1;
-
-    if (that.getSwAccession() == this.getSwAccession()) // when both names are
-                                                        // null
-      return 0;
-
-    if (that.getSwAccession() == null)
-      return -1; // when only the other name is null
-
-    return (that.getSwAccession().compareTo(this.getSwAccession()));
-  }
-
-  /** {@inheritDoc} */
-  @Override
-  public String toString() {
-    return new ToStringBuilder(this).append("swAccession", getSwAccession()).toString();
-  }
-
-  /** {@inheritDoc} */
-  @Override
-  public boolean equals(Object other) {
-    if ((this == other))
-      return true;
-    if (!(other instanceof Expense))
-      return false;
-    Expense castOther = (Expense) other;
-    return new EqualsBuilder().append(this.getSwAccession(), castOther.getSwAccession()).isEquals();
-  }
-  
-  /** {@inheritDoc} */
-  @Override
-  public boolean givesPermission(Registration registration) {
-    boolean hasPermission = true;
-    if (registration == null) {
-      hasPermission = false;
-    }
-
-    if (!hasPermission) {
-      Logger.getLogger(Workflow.class).info("Expense does not give permission");
-      throw new SecurityException("User " + registration.getEmailAddress()
-          + " does not have permission to modify aspects of invoice " + this.getSwAccession());
-    } else {
-      Logger.getLogger(Workflow.class).info("Expenses are public by default");
-    }
-    return hasPermission;
-  }
-
-  /** {@inheritDoc} */
-  @Override
-  public int hashCode() {
-    return new HashCodeBuilder().append(getSwAccession()).toHashCode();
-  }
+    private static final long serialVersionUID = 1L;
+    private Integer expenseId;
+    private Invoice invoice;
+    private WorkflowRun workflowRun;
+    private String agent;
+    private String expenseType;
+    private String description;
+    private Double pricePerUnit;
+    private Double totalUnits;
+    private Double totalPrice;
+    private Double addedSurcharge;
+    private Integer swAccession;
+    private Date finishedTimestamp;
+    private Logger logger;
+    //private Set<ExpenseAttribute> invoiceAttributes = new TreeSet<ExpenseAttribute>();
 
     /**
-     * <p>Getter for the field <code>expenseId</code>.</p>
+     * <p>Constructor for Expense.</p>
+     */
+    public Expense() {
+        super();
+        logger = Logger.getLogger(Expense.class);
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public int compareTo(Expense that) {
+        if (that == null) {
+            return -1;
+        }
+
+        if (that.getSwAccession() == this.getSwAccession()) // when both names are
+        // null
+        {
+            return 0;
+        }
+
+        if (that.getSwAccession() == null) {
+            return -1; // when only the other name is null
+        }
+        return (that.getSwAccession().compareTo(this.getSwAccession()));
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public String toString() {
+        return new ToStringBuilder(this).append("swAccession", getSwAccession()).toString();
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public boolean equals(Object other) {
+        if ((this == other)) {
+            return true;
+        }
+        if (!(other instanceof Expense)) {
+            return false;
+        }
+        Expense castOther = (Expense) other;
+        return new EqualsBuilder().append(this.getSwAccession(), castOther.getSwAccession()).isEquals();
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public boolean givesPermission(Registration registration) {
+        boolean hasPermission = true;
+        if (registration == null) {
+            hasPermission = false;
+        }
+
+        if (!hasPermission) {
+            Logger.getLogger(Workflow.class).info("Expense does not give permission");
+            throw new SecurityException("User " + registration.getEmailAddress()
+                    + " does not have permission to modify aspects of invoice " + this.getSwAccession());
+        } else {
+            Logger.getLogger(Workflow.class).info("Expenses are public by default");
+        }
+        return hasPermission;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public int hashCode() {
+        return new HashCodeBuilder().append(getSwAccession()).toHashCode();
+    }
+
+    /**
+     * <p>Getter for the field
+     * <code>expenseId</code>.</p>
      *
      * @return a {@link java.lang.Integer} object.
      */
@@ -113,7 +130,8 @@ public class Expense implements Serializable, Comparable<Expense>, PermissionsAw
     }
 
     /**
-     * <p>Setter for the field <code>expenseId</code>.</p>
+     * <p>Setter for the field
+     * <code>expenseId</code>.</p>
      *
      * @param expenseId a {@link java.lang.Integer} object.
      */
@@ -122,7 +140,8 @@ public class Expense implements Serializable, Comparable<Expense>, PermissionsAw
     }
 
     /**
-     * <p>Getter for the field <code>invoice</code>.</p>
+     * <p>Getter for the field
+     * <code>invoice</code>.</p>
      *
      * @return a {@link net.sourceforge.seqware.common.model.Invoice} object.
      */
@@ -131,34 +150,41 @@ public class Expense implements Serializable, Comparable<Expense>, PermissionsAw
     }
 
     /**
-     * <p>Setter for the field <code>invoice</code>.</p>
+     * <p>Setter for the field
+     * <code>invoice</code>.</p>
      *
-     * @param invoice a {@link net.sourceforge.seqware.common.model.Invoice} object.
+     * @param invoice a {@link net.sourceforge.seqware.common.model.Invoice}
+     * object.
      */
     public void setInvoice(Invoice invoice) {
         this.invoice = invoice;
     }
 
     /**
-     * <p>Getter for the field <code>workflowRun</code>.</p>
+     * <p>Getter for the field
+     * <code>workflowRun</code>.</p>
      *
-     * @return a {@link net.sourceforge.seqware.common.model.WorkflowRun} object.
+     * @return a {@link net.sourceforge.seqware.common.model.WorkflowRun}
+     * object.
      */
     public WorkflowRun getWorkflowRun() {
         return workflowRun;
     }
 
     /**
-     * <p>Setter for the field <code>workflowRun</code>.</p>
+     * <p>Setter for the field
+     * <code>workflowRun</code>.</p>
      *
-     * @param workflowRun a {@link net.sourceforge.seqware.common.model.WorkflowRun} object.
+     * @param workflowRun a
+     * {@link net.sourceforge.seqware.common.model.WorkflowRun} object.
      */
     public void setWorkflowRun(WorkflowRun workflowRun) {
         this.workflowRun = workflowRun;
     }
 
     /**
-     * <p>Getter for the field <code>agent</code>.</p>
+     * <p>Getter for the field
+     * <code>agent</code>.</p>
      *
      * @return a {@link java.lang.String} object.
      */
@@ -167,7 +193,8 @@ public class Expense implements Serializable, Comparable<Expense>, PermissionsAw
     }
 
     /**
-     * <p>Setter for the field <code>agent</code>.</p>
+     * <p>Setter for the field
+     * <code>agent</code>.</p>
      *
      * @param agent a {@link java.lang.String} object.
      */
@@ -176,7 +203,8 @@ public class Expense implements Serializable, Comparable<Expense>, PermissionsAw
     }
 
     /**
-     * <p>Getter for the field <code>expenseType</code>.</p>
+     * <p>Getter for the field
+     * <code>expenseType</code>.</p>
      *
      * @return a {@link java.lang.String} object.
      */
@@ -185,7 +213,8 @@ public class Expense implements Serializable, Comparable<Expense>, PermissionsAw
     }
 
     /**
-     * <p>Setter for the field <code>expenseType</code>.</p>
+     * <p>Setter for the field
+     * <code>expenseType</code>.</p>
      *
      * @param expenseType a {@link java.lang.String} object.
      */
@@ -194,7 +223,8 @@ public class Expense implements Serializable, Comparable<Expense>, PermissionsAw
     }
 
     /**
-     * <p>Getter for the field <code>description</code>.</p>
+     * <p>Getter for the field
+     * <code>description</code>.</p>
      *
      * @return a {@link java.lang.String} object.
      */
@@ -203,7 +233,8 @@ public class Expense implements Serializable, Comparable<Expense>, PermissionsAw
     }
 
     /**
-     * <p>Setter for the field <code>description</code>.</p>
+     * <p>Setter for the field
+     * <code>description</code>.</p>
      *
      * @param description a {@link java.lang.String} object.
      */
@@ -212,16 +243,28 @@ public class Expense implements Serializable, Comparable<Expense>, PermissionsAw
     }
 
     /**
-     * <p>Getter for the field <code>pricePerUnit</code>.</p>
+     * <p>Getter for the field
+     * <code>pricePerUnit</code>.</p>
      *
      * @return a {@link java.lang.Double} object.
      */
     public Double getPricePerUnit() {
         return pricePerUnit;
     }
+    
+    /**
+     * <p>Getter for the field
+     * <code>pricePerUnit</code>.</p>
+     *
+     * @return a {@link java.lang.String} object.
+     */
+    public String getPricePerUnitString() {
+        return round(pricePerUnit);
+    }
 
     /**
-     * <p>Setter for the field <code>pricePerUnit</code>.</p>
+     * <p>Setter for the field
+     * <code>pricePerUnit</code>.</p>
      *
      * @param pricePerUnit a {@link java.lang.Double} object.
      */
@@ -230,7 +273,8 @@ public class Expense implements Serializable, Comparable<Expense>, PermissionsAw
     }
 
     /**
-     * <p>Getter for the field <code>totalUnits</code>.</p>
+     * <p>Getter for the field
+     * <code>totalUnits</code>.</p>
      *
      * @return a {@link java.lang.Double} object.
      */
@@ -239,7 +283,8 @@ public class Expense implements Serializable, Comparable<Expense>, PermissionsAw
     }
 
     /**
-     * <p>Setter for the field <code>totalUnits</code>.</p>
+     * <p>Setter for the field
+     * <code>totalUnits</code>.</p>
      *
      * @param totalUnits a {@link java.lang.Double} object.
      */
@@ -248,7 +293,8 @@ public class Expense implements Serializable, Comparable<Expense>, PermissionsAw
     }
 
     /**
-     * <p>Getter for the field <code>totalPrice</code>.</p>
+     * <p>Getter for the field
+     * <code>totalPrice</code>.</p>
      *
      * @return a {@link java.lang.Double} object.
      */
@@ -257,7 +303,23 @@ public class Expense implements Serializable, Comparable<Expense>, PermissionsAw
     }
 
     /**
-     * <p>Setter for the field <code>totalPrice</code>.</p>
+     * <p>Getter for the field
+     * <code>totalPrice</code>.</p>
+     *
+     * @return a {@link java.lang.Double} object.
+     */
+    public String getTotalPriceString() {
+        return round(totalPrice);
+    }
+
+    private String round(Double input) {
+        BigDecimal bd = new BigDecimal(input);
+        return (bd.setScale(2, BigDecimal.ROUND_HALF_EVEN).toString());
+    }
+
+    /**
+     * <p>Setter for the field
+     * <code>totalPrice</code>.</p>
      *
      * @param totalPrice a {@link java.lang.Double} object.
      */
@@ -266,7 +328,8 @@ public class Expense implements Serializable, Comparable<Expense>, PermissionsAw
     }
 
     /**
-     * <p>Getter for the field <code>addedSurcharge</code>.</p>
+     * <p>Getter for the field
+     * <code>addedSurcharge</code>.</p>
      *
      * @return a {@link java.lang.Double} object.
      */
@@ -275,7 +338,8 @@ public class Expense implements Serializable, Comparable<Expense>, PermissionsAw
     }
 
     /**
-     * <p>Setter for the field <code>addedSurcharge</code>.</p>
+     * <p>Setter for the field
+     * <code>addedSurcharge</code>.</p>
      *
      * @param addedSurcharge a {@link java.lang.Double} object.
      */
@@ -284,7 +348,8 @@ public class Expense implements Serializable, Comparable<Expense>, PermissionsAw
     }
 
     /**
-     * <p>Getter for the field <code>swAccession</code>.</p>
+     * <p>Getter for the field
+     * <code>swAccession</code>.</p>
      *
      * @return a {@link java.lang.Integer} object.
      */
@@ -293,7 +358,8 @@ public class Expense implements Serializable, Comparable<Expense>, PermissionsAw
     }
 
     /**
-     * <p>Setter for the field <code>swAccession</code>.</p>
+     * <p>Setter for the field
+     * <code>swAccession</code>.</p>
      *
      * @param swAccession a {@link java.lang.Integer} object.
      */
@@ -302,7 +368,8 @@ public class Expense implements Serializable, Comparable<Expense>, PermissionsAw
     }
 
     /**
-     * <p>Getter for the field <code>finishedTimestamp</code>.</p>
+     * <p>Getter for the field
+     * <code>finishedTimestamp</code>.</p>
      *
      * @return a {@link java.util.Date} object.
      */
@@ -311,13 +378,12 @@ public class Expense implements Serializable, Comparable<Expense>, PermissionsAw
     }
 
     /**
-     * <p>Setter for the field <code>finishedTimestamp</code>.</p>
+     * <p>Setter for the field
+     * <code>finishedTimestamp</code>.</p>
      *
      * @param finishedTimestamp a {@link java.util.Date} object.
      */
     public void setFinishedTimestamp(Date finishedTimestamp) {
         this.finishedTimestamp = finishedTimestamp;
     }
-  
-  
 }
--- seqware-portal/src/main/webapp/WEB-INF/jsp/invoiceDetails.jsp
+++ seqware-portal/src/main/webapp/WEB-INF/jsp/invoiceDetails.jsp
@@ -26,72 +26,88 @@
     <p><b>Invoice is Paid</b>: ${invoice.fullyPaid}</p>
     
     <p><b>Notes to Client</b>:</p><p>${invoice.notes}</p>
-
-    <h3>Fixed Monthly Expenses</h3>
     
+    <link rel="stylesheet" type="text/css" href="styles/jquery-ui.css"/>
+    <script type="text/javascript" src="scripts/jquery-1.8.2.js"></script>
+    <script type="text/javascript" src="scripts/jquery-ui.js"></script>
+    
+    <script>
+    $(function() {
+        $( "#accordion" ).accordion({
+            heightStyle: "content",
+            active: false,
+            collapsible: true
+        });
+    });
+    </script>
+
+    <div id="accordion">
+    <h3>
+      Fixed Monthly Expenses
+      <table class="m-table-base">
+        <tr><th style="width: 80%">Fixed Expense Total Price</th><th>$${fixed_total_price}</th></tr>
+      </table>
+    </h3>
+    <div>
     <c:if test ="${fixed_size == 0}">
         No fixed expenses currently reported.<p/>
     </c:if>
-
     <c:forEach items="${fixed}" var="expense">
         <table class="m-table-base">
             <tr><th style="width: 80%">Item (SWID:${expense.swAccession})</th><th>Price</th></tr>
             <tr><td>${expense.description} [${expense.agent}]</td><td></td></tr>
-            <tr><td>${expense.totalUnits} units @ $${expense.pricePerUnit} per unit = </td><td>$${expense.totalPrice}</td></tr>
+            <tr><td>${expense.totalUnits} units @ $${expense.pricePerUnitString} per unit = </td><td>$${expense.totalPriceString}</td></tr>
         </table>
         <p/>
-    </c:forEach>    
-        <p>
-            <table class="m-table-base">
-            <tr><th style="width: 80%">Fixed Expense Total Price</th><th>$${fixed_total_price}</th></tr>
-            </table>
-        </p>
-    
-    <h3>Consulting Expenses</h3>
-    
+    </c:forEach>   
+    </div>
+    <h3>
+        Consulting Expenses
+        <table class="m-table-base">
+        <tr><th style="width: 80%">Consulting Expense Total Price</th><th>$${consulting_total_price}</th></tr>
+        </table>
+    </h3>
+    <div>
     <c:if test ="${consulting_size == 0}">
         No consulting expenses currently reported. <p/>
     </c:if>
-    
-     <c:forEach items="${consulting}" var="expense">
+    <c:forEach items="${consulting}" var="expense">
         <table class="m-table-base">
             <tr><th style="width: 80%">Item (SWID:${expense.swAccession})</th><th>Price</th></tr>
             <tr><td>${expense.description} [${expense.agent}]</td><td></td></tr>
-            <tr><td>${expense.totalUnits} units @ $${expense.pricePerUnit} per unit = </td><td>$${expense.totalPrice}</td></tr>
+            <tr><td>${expense.totalUnits} units @ $${expense.pricePerUnitString} per unit = </td><td>$${expense.totalPriceString}</td></tr>
         </table>
         <p/>
     </c:forEach> 
-                <p>
-            <table class="m-table-base">
-            <tr><th style="width: 80%">Consulting Expense Total Price</th><th>$${consulting_total_price}</th></tr>
-             </table>
-        </p>
-    
-    <h3>Analysis and Storage Expenses</h3>
-    
+    </div>
+    <h3>
+        Analysis and Storage Expenses
+        <table class="m-table-base">
+        <tr><th style="width: 80%">Analysis Expense Total Price</th><th>$${analysis_total_price}</th></tr>
+        </table>
+    </h3>
+    <div>
     <c:if test ="${analysis_size == 0}">
         No analysis expenses currently reported.<p/>
     </c:if>
-    
     <c:forEach items="${analysis}" var="expense">
         <table class="m-table-base">
             <tr><th style="width: 80%">Item (SWID:${expense.swAccession})</th><th>Price</th></tr>
             <tr><td>${expense.description} [${expense.agent}]</td><td></td></tr>
-            <tr><td>${expense.totalUnits} units @ $${expense.pricePerUnit} per unit</td><td>$${expense.totalPrice}</td></tr>
+            <tr><td>${expense.totalUnits} units @ $${expense.pricePerUnitString} per unit</td><td>$${expense.totalPriceString}</td></tr>
         </table>
         <p/> 
     </c:forEach> 
-        <p>
-            <table class="m-table-base">
-            <tr><th style="width: 80%">Analysis Expense Total Price</th><th>$${analysis_total_price}</th></tr>
-            </table>
-        </p>
+    </div>
+        
+    <!-- end accordion -->
+    </div>
     
         <p>
     <table>
         <tr><td><b>Total:</b></td><td>$${total_price}</td></tr>
         <tr><td><b>Previously Paid:</b></td><td>$${paid_amount}</td></tr>
-        <tr><td><b>Total Due:</b></td><td>$${total_due}</td></tr>
+        <tr><td><b>Total Due:</b></td><td>$${total_due_currency}</td></tr>
         <tr><td><b>Please Pay Within:</b></td><td>${invoice.daysUntilDue} days</td></tr>
     </table>
 </p>
@@ -128,7 +144,7 @@
 <input type="hidden" name="cn" value="Add special instructions to the seller:">
 <input type="hidden" name="no_shipping" value="2">
 <input type="hidden" name="currency_code" value="USD">
-<input type="hidden" name="amount" value="${total_due_currency}">
+<input type="hidden" name="amount" value="$${total_due_currency}">
 <input type="hidden" name="bn" value="PP-BuyNowBF:btn_paynowCC_LG.gif:NonHosted">
 <input type="image" src="https://www.paypalobjects.com/en_US/i/btn/btn_paynowCC_LG.gif" border="0" name="submit" alt="PayPal - The safer, easier way to pay online!">
 <img alt="" border="0" src="https://www.paypalobjects.com/en_US/i/scr/pixel.gif" width="1" height="1">
