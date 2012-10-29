e85d377a77eb3aed913e207542915fbd3c252521
Denis Yuen <dyuen@oicr.on.ca>
2012-10-29 14:55:49 -0400

    Updating tests for web service after Brian's changes to work.

--- seqware-webservice/src/test/java/net/sourceforge/seqware/webservice/resources/tables/LaneResourceTest.java
+++ seqware-webservice/src/test/java/net/sourceforge/seqware/webservice/resources/tables/LaneResourceTest.java
@@ -16,15 +16,42 @@
  */
 package net.sourceforge.seqware.webservice.resources.tables;
 
+import java.util.Date;
+import junit.framework.Assert;
+import net.sourceforge.seqware.common.model.IUS;
+import net.sourceforge.seqware.common.model.Lane;
+import net.sourceforge.seqware.common.model.Sample;
+import net.sourceforge.seqware.common.util.xmltools.JaxbObject;
+import net.sourceforge.seqware.common.util.xmltools.XmlTools;
+import org.restlet.representation.Representation;
+import org.restlet.representation.StringRepresentation;
+import org.w3c.dom.Document;
+
 /**
  *
  * @author mtaschuk
  */
-public class LaneResourceTest extends DatabaseResourceTest{
+public class LaneResourceTest extends DatabaseResourceTest {
 
     public LaneResourceTest() {
         super("/lanes");
     }
-    
-    
+
+    @Override
+    public void testPost() {
+        System.out.println(getRelativeURI() + " POST");
+        Representation rep = null;
+        try {
+            Lane l = new Lane();
+            l.setLaneId(1701);
+
+            Document doc = XmlTools.marshalToDocument(new JaxbObject<Lane>(), l);
+            rep = resource.post(XmlTools.getRepresentation(doc));
+            rep.exhaust();
+            rep.release();
+        } catch (Exception e) {
+            e.printStackTrace();
+            Assert.fail(e.getMessage());
+        }
+    }
 }
