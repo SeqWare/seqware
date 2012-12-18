4ba6aa1620e01ee9019ecc837222f10b0702698c
Brian O'Connor <briandoconnor@gmail.com>
2012-12-18 18:15:07 -0500

    Trying to construct the right params to ProvisionFiles
    Former-commit-id: df055d473a7ad2b59bf9a4d391006f060c088151

--- seqware-pipeline/src/main/java/net/sourceforge/seqware/pipeline/workflowV2/engine/oozie/object/OozieProvisionFileJob.java
+++ seqware-pipeline/src/main/java/net/sourceforge/seqware/pipeline/workflowV2/engine/oozie/object/OozieProvisionFileJob.java
@@ -95,7 +95,7 @@ public class OozieProvisionFileJob extends OozieJob {
 		String output = this.outputDir;
 		if(this.file.isOutput()) {
 			inputArg = "--input-file-metadata";
-			inputValue =  this.jobObj.getAlgo() + "::" + this.file.getType() + "::" + this.outputDir + "/" + this.file.getSourcePath();
+			inputValue =  this.jobObj.getAlgo() + "::" + this.file.getType() + "::" + this.getOozieWorkingDir() + "/" + this.file.getSourcePath();
 			output = this.metadataOutputPrefix + "/" + this.outputDir;
 		}
 		Element inputTE = new Element("arg", WorkflowApp.NAMESPACE);
