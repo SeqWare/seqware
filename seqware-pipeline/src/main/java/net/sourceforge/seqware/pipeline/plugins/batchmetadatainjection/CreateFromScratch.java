cfa2c6d449faa51a533060376be46e689666c738
Morgan Taschuk <mtaschuk@oicr.on.ca>
2013-03-26 14:47:56 -0400

    Addressing bugs PDE-146, PDE-147, PDE-148. fix the 'press enter to skip' problem; there are two numbers listed for organism; require that the program ask again for different samples. Currently refactoring to deal with integer indexes properly
    Former-commit-id: af9307c0363aa1b407f7a24be9275d6a9f26fcfe

--- seqware-pipeline/src/main/java/net/sourceforge/seqware/pipeline/plugins/batchmetadatainjection/CreateFromScratch.java
+++ seqware-pipeline/src/main/java/net/sourceforge/seqware/pipeline/plugins/batchmetadatainjection/CreateFromScratch.java
@@ -20,6 +20,7 @@ import java.util.Map;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import net.sourceforge.seqware.common.metadata.Metadata;
+import net.sourceforge.seqware.common.util.Log;
 import net.sourceforge.seqware.common.util.runtools.ConsoleAdapter;
 
 /**
@@ -27,37 +28,52 @@ import net.sourceforge.seqware.common.util.runtools.ConsoleAdapter;
  * @author mtaschuk
  */
 public class CreateFromScratch extends BatchMetadataParser {
-
+    
     public CreateFromScratch(Metadata metadata, Map<String, String> fields, boolean interactive) {
         super(metadata, fields, interactive);
     }
     
     public RunInfo getRunInfo() {
         RunInfo runInfo = this.generateRunInfo(null, null, null, null, null, null, null, null, null, -1, -1, true, null, null);
-        String projectCode="ZZZZ";
+        String projectCode = "ZZZZ";
         
         int numLanes = this.prompt("Number of lanes?", 8, Field.number_of_lanes);
         
-        for (int i = 0; i<numLanes; i++){
-            LaneInfo lane = this.generateLaneInfo(null, -1);
-            
-            int numBarcodes = this.prompt("How many barcodes in lane "+lane.getLaneNumber()+"?", 1, Field.number_of_barcodes_per_lane);
-            for (int j=0; j<numBarcodes; j++) {
+        for (int i = 0; i < numLanes; i++) {
+            LaneInfo lane = getLane(i);
+            int numBarcodes = this.prompt("How many barcodes in lane " + lane.getLaneNumber() + "?", 1, null);
+            for (int j = 0; j < numBarcodes; j++) {
                 try {
-                    projectCode = ConsoleAdapter.getInstance().promptString("Project code (three or four letters)", projectCode);
-                    String individualNumber = ConsoleAdapter.getInstance().promptString("Individual number (should be unique per project)", null);
-                    SampleInfo sample = this.generateSampleInfo(null, projectCode, individualNumber, 
-                            null, null, null, null, null, null, -1, null, null, null, null, null);
+                    boolean done = false;
+                    SampleInfo sample = null;
+                    while (!done) {
+                        projectCode = ConsoleAdapter.getInstance().promptString("Project code (three or four letters)", projectCode);
+                        String individualNumber = ConsoleAdapter.getInstance().promptString("Individual number (should be unique per project)", null);
+                        sample = this.generateSampleInfo(projectCode + " " + individualNumber, projectCode, individualNumber,
+                                null, null, null, null, null, null, -1, null, null, null, null, null);
+                        Log.stdout(sample.toString());
+                        done = ConsoleAdapter.getInstance().promptBoolean("Is this correct?", true);
+                    }
                     lane.getSamples().add(sample);
                     
                 } catch (Exception ex) {
                     Logger.getLogger(CreateFromScratch.class.getName()).log(Level.SEVERE, null, ex);
                 }
             }
-            runInfo.getLanes().add(lane);   
-        }        
+            runInfo.getLanes().add(lane);
+        }
         
         return runInfo;
     }
-    
+
+    private LaneInfo getLane(int i) {
+        boolean doneLane = false;
+        LaneInfo lane = null;
+        while (!doneLane) {
+            lane = this.generateLaneInfo(String.valueOf(i + 1), -1);
+            Log.stdout(lane.toString());
+            doneLane = ConsoleAdapter.getInstance().promptBoolean("Is this correct?", true);
+        }
+        return lane;
+    }
 }
