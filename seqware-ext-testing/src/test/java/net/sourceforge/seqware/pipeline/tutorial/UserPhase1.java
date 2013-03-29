088c10a493b6f6b14cc35ef44faa864de810b35a
Denis Yuen <denis.yuen@oicr.on.ca>
2013-03-28 22:29:04 -0400

    Working integration tests that cover the user tutorial in order as well as some of the developer tutorial and admin tutorial.
    Total runtime is 73 minutes, 16 seconds, also requires roughly 10GB of space. Some of this is in /tmp and has to be cleaned in the next iteration.
    Run via the steps in the README, make sure to correctly set your .seqware/settings to hit the Jetty webservice during tests and turn off any cronjob entries relating to SeqWare.

---
+++ seqware-ext-testing/src/test/java/net/sourceforge/seqware/pipeline/tutorial/UserPhase1.java
@@ -0,0 +1,55 @@
+/*
+ * Copyright (C) 2013 SeqWare
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package net.sourceforge.seqware.pipeline.tutorial;
+
+import java.io.IOException;
+import junit.framework.Assert;
+import net.sourceforge.seqware.common.module.ReturnValue;
+import net.sourceforge.seqware.pipeline.plugins.ITUtility;
+import org.junit.Test;
+
+/**
+ * Do all tests that can be concurrently done in the user tutorial
+ * @author dyuen
+ */
+public class UserPhase1 {
+    
+    public static final String STUDY = "study";
+    
+    @Test
+    public void testMetadataListTables() throws IOException {
+        String output = ITUtility.runSeqWareJar(" -p net.sourceforge.seqware.pipeline.plugins.Metadata -- --list-tables", ReturnValue.SUCCESS);
+        Assert.assertTrue("output should include table names", output.contains("TableName") && output.contains("study") && output.contains("experiment"));
+        Assert.assertTrue("output should include table names", output.contains("sample") && output.contains("sequencer_run") && output.contains("ius") && output.contains("lane"));
+    }
+    
+    @Test
+    public void testMetadataListFields() throws IOException {
+        String output = ITUtility.runSeqWareJar(" -p net.sourceforge.seqware.pipeline.plugins.Metadata -- --table study --list-fields", ReturnValue.SUCCESS);
+        Assert.assertTrue("output should include column names", output.contains("Field\tType\tPossible_Values"));
+        Assert.assertTrue("output should include field names", output.contains("title\tString"));
+        Assert.assertTrue("output should include field names", output.contains("study_type\tInteger"));
+    }
+    
+    @Test
+    public void testStudyCreation() throws IOException {
+        String output = ITUtility.runSeqWareJar(" -p net.sourceforge.seqware.pipeline.plugins.Metadata -- --table study --create --field title::New Test Study --field description::This is a test description --field accession::InternalID123 --field center_name::SeqWare --field center_project_name::SeqWare Test Project --field study_type::4", ReturnValue.SUCCESS);
+        String sw_accession  = UserTutorialSuiteIT.getAndCheckSwid(output);
+        AccessionMap.accessionMap.put(STUDY, sw_accession);
+    }
+    
+}
