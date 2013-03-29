a5540c65af788124cae5c8711d955dc051f3afb3
Denis Yuen <denis.yuen@oicr.on.ca>
2013-03-28 22:29:04 -0400

    Working integration tests that cover the user tutorial in order as well as some of the developer tutorial and admin tutorial.
    Total runtime is 73 minutes, 16 seconds, also requires roughly 10GB of space. Some of this is in /tmp and has to be cleaned in the next iteration.
    Run via the steps in the README, make sure to correctly set your .seqware/settings to hit the Jetty webservice during tests and turn off any cronjob entries relating to SeqWare.
    
    
    Former-commit-id: 97d0b7b2178d41cca9c0f1531861fa5613dfed08

---
+++ seqware-ext-testing/src/test/java/net/sourceforge/seqware/pipeline/tutorial/UserAndDeveloperPhase5.java
@@ -0,0 +1,69 @@
+/*
+ * Copyright (C) 2013 SeqWare
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package net.sourceforge.seqware.pipeline.tutorial;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import junit.framework.Assert;
+import net.sourceforge.seqware.common.module.ReturnValue;
+import net.sourceforge.seqware.common.util.Log;
+import net.sourceforge.seqware.pipeline.plugins.ITUtility;
+import net.sourceforge.seqware.pipeline.plugins.PluginRunnerIT;
+import net.sourceforge.seqware.pipeline.runner.PluginRunner;
+import org.junit.Test;
+
+/**
+ * Build and install a bundle, used by both the User tutorial and the Developer tutorial
+ * @author dyuen
+ */
+public class UserAndDeveloperPhase5 {
+    
+    public static final String WORKFLOW = "Workflow";
+    
+    @Test
+    public void testListAvailableWorkflowsAndTheirParameters() throws IOException {
+        PluginRunnerIT pit = new PluginRunnerIT();
+        PluginRunnerIT.clearStaticVariables();
+        PluginRunner it = new PluginRunner();
+        String SEQWARE_VERSION = it.getClass().getPackage().getImplementationVersion();
+        Assert.assertTrue("unable to detect seqware version", SEQWARE_VERSION != null);
+        Log.info("SeqWare version detected as: " + SEQWARE_VERSION);
+
+        // for all tests, we're going to need to create and install our basic archetypes
+        String[] archetypes = {"java-workflow"};
+        PluginRunnerIT.buildAndInstallArchetypes(archetypes, SEQWARE_VERSION);
+
+        //list workflows and ensure that the workflow is installed
+        List<Integer> accessions = new ArrayList<Integer>();
+        accessions.addAll(PluginRunnerIT.getInstalledWorkflows().values());
+        Assert.assertTrue("one accession expected", accessions.size() == 1);
+        AccessionMap.accessionMap.put(WORKFLOW, accessions.get(0).toString());
+
+        // launch our specific workflow and get store its workflow run accession
+        File exportINIFile = pit.exportINIFile("Java workflow", accessions.get(0));
+        
+        String localhost = ITUtility.getLocalhost();
+        Log.info("Attempting to launch with wait on host: " + localhost);
+        // launch, slightly unlike the tutorial, I'm going to wait to ensure that we have results to export in the next phase
+        String listCommand = "-p net.sourceforge.seqware.pipeline.plugins.WorkflowLauncher -- --ini-files " + exportINIFile.getAbsolutePath() + " --workflow-accession " + accessions.get(0)
+                + " --parent-accessions " + AccessionMap.accessionMap.get(UserPhase4.FILE) + " --wait --host " + localhost;
+        String listOutput = ITUtility.runSeqWareJar(listCommand, ReturnValue.SUCCESS);
+        Log.info(listOutput);
+    }
+}
