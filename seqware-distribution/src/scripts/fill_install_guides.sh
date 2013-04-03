d3748229016a63e3dfec41f2128f89625e622e3f
Denis Yuen <denis.yuen@gmail.com>
2013-04-03 19:17:20 -0400

    Remove redundant script
    Former-commit-id: 4fd2e19ec84d29cb35ff6320f3356de8b1ffb14b

--- seqware-distribution/src/scripts/fill_install_guides.sh
+++
@@ -1,8 +0,0 @@
-#! /bin/bash
-git checkout docs/site/content/docs/github_readme/
-git update-index --assume-unchanged docs/site/content/docs/github_readme/*.md
-cat ../seqware-portal/README.md >> docs/site/content/docs/github_readme/5-portal.md
-cat ../seqware-pipeline/README.md >> docs/site/content/docs/github_readme/1-pipeline.md
-cat ../seqware-meta-db/README.md >> docs/site/content/docs/github_readme/3-metadb.md
-cat ../seqware-queryengine/README.md >> docs/site/content/docs/github_readme/2-queryengine.md
-cat ../seqware-webservice/README.md >> docs/site/content/docs/github_readme/4-webservice.md
