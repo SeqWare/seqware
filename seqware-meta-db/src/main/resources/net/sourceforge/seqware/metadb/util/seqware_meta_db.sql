85654f21a1e2aa175c2aefa15b0b75c66a6265e9
Denis Yuen <denis.yuen@oicr.on.ca>
2013-06-11 13:57:04 -0400

    Update database scripts with supporting indices for SymLinkFileReporter, SequencerRunReporter, and BasicDecider

--- seqware-meta-db/src/main/resources/net/sourceforge/seqware/metadb/util/seqware_meta_db.sql
+++ seqware-meta-db/src/main/resources/net/sourceforge/seqware/metadb/util/seqware_meta_db.sql
@@ -4036,6 +4036,34 @@ ALTER TABLE ONLY workflow_run
 
 
 --
+-- Name: ancestor_workflow_run_processing; Type: INDEX; Schema: public; Owner: seqware; Tablespace: 
+--
+
+CREATE INDEX ancestor_workflow_run_processing ON processing USING btree (ancestor_workflow_run_id);
+
+
+--
+-- Name: email_registration; Type: INDEX; Schema: public; Owner: seqware; Tablespace: 
+--
+
+CREATE INDEX email_registration ON registration USING btree (email);
+
+
+--
+-- Name: experiment_id_sample; Type: INDEX; Schema: public; Owner: seqware; Tablespace: 
+--
+
+CREATE INDEX experiment_id_sample ON sample USING btree (experiment_id);
+
+
+--
+-- Name: file_id_processing_files; Type: INDEX; Schema: public; Owner: seqware; Tablespace: 
+--
+
+CREATE INDEX file_id_processing_files ON processing_files USING btree (file_id);
+
+
+--
 -- Name: file_report_study_id_idx; Type: INDEX; Schema: public; Owner: seqware; Tablespace: 
 --
 
@@ -4055,12 +4083,96 @@ CREATE INDEX index_processing_relationship_child_id ON processing_relationship U
 
 CREATE INDEX index_processing_relationship_parent_id ON processing_relationship USING btree (parent_id);
 
+--
+-- Name: ius_id_ius_workflow_runs; Type: INDEX; Schema: public; Owner: seqware; Tablespace: 
+--
+
+CREATE INDEX ius_id_ius_workflow_runs ON ius_workflow_runs USING btree (ius_id);
+
+
+--
+-- Name: parent_sample_hierarchy; Type: INDEX; Schema: public; Owner: seqware; Tablespace: 
+--
+
+CREATE INDEX parent_sample_hierarchy ON sample_hierarchy USING btree (parent_id);
+
+
+--
+-- Name: processing_id_processing_files; Type: INDEX; Schema: public; Owner: seqware; Tablespace: 
+--
+
+CREATE INDEX processing_id_processing_files ON processing_files USING btree (processing_id);
+
+
+--
+-- Name: sample_id_ius; Type: INDEX; Schema: public; Owner: seqware; Tablespace: 
+--
+
+CREATE INDEX sample_id_ius ON ius USING btree (sample_id);
+
+
+--
+-- Name: sample_id_processing_samples; Type: INDEX; Schema: public; Owner: seqware; Tablespace: 
+--
+
+CREATE INDEX sample_id_processing_samples ON processing_samples USING btree (sample_id);
+
+
+--
+-- Name: sample_id_sample_attribute; Type: INDEX; Schema: public; Owner: seqware; Tablespace: 
+--
+
+CREATE INDEX sample_id_sample_attribute ON sample_attribute USING btree (sample_id);
+
 
 --
 -- Name: sample_report_study_id_child_sample_id_workflow_id_idx; Type: INDEX; Schema: public; Owner: seqware; Tablespace: 
 --
 
 CREATE INDEX sample_report_study_id_child_sample_id_workflow_id_idx ON sample_report USING btree (study_id, child_sample_id, workflow_id);
+
+
+--
+-- Name: status_workflow_run; Type: INDEX; Schema: public; Owner: seqware; Tablespace: 
+--
+
+CREATE INDEX status_workflow_run ON workflow_run USING btree (status);
+
+
+--
+-- Name: sw_accession_file; Type: INDEX; Schema: public; Owner: seqware; Tablespace: 
+--
+
+CREATE INDEX sw_accession_file ON file USING btree (sw_accession);
+
+
+--
+-- Name: sw_accession_processing; Type: INDEX; Schema: public; Owner: seqware; Tablespace: 
+--
+
+CREATE UNIQUE INDEX sw_accession_processing ON processing USING btree (sw_accession);
+
+
+--
+-- Name: sw_accession_workflow_run; Type: INDEX; Schema: public; Owner: seqware; Tablespace: 
+--
+
+CREATE UNIQUE INDEX sw_accession_workflow_run ON workflow_run USING btree (sw_accession);
+
+
+--
+-- Name: workflow_run_id_ius_workflow_runs; Type: INDEX; Schema: public; Owner: seqware; Tablespace: 
+--
+
+CREATE INDEX workflow_run_id_ius_workflow_runs ON ius_workflow_runs USING btree (workflow_run_id);
+
+
+--
+-- Name: workflow_run_processing; Type: INDEX; Schema: public; Owner: seqware; Tablespace: 
+--
+
+CREATE INDEX workflow_run_processing ON processing USING btree (workflow_run_id);
+
 
 
 --
