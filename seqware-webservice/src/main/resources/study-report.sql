ef825bf450525c958d77a591af98a76a9eb3a04a
Alexander Taggart <Alexander.Taggart@oicr.on.ca>
2013-06-19 12:53:41 -0400

    Test-ready impl of new study report
    Former-commit-id: f3eda4fd9382e04ca532532b0727a41541c4d5d4

--- seqware-webservice/src/main/resources/study-report.sql
+++ seqware-webservice/src/main/resources/study-report.sql
@@ -1,70 +1,91 @@
-
--- tuples of leaf samples and the child/parent walk up to the root sample(s)
--- first tuple is bootstrapped as (leaf, leaf, leaf's parent)
-with recursive sample_ancestors(leaf_id, child_id, parent_id) as (
-    select c.sample_id, h.sample_id, h.parent_id
-    from (select distinct sample_id from sample_hierarchy
-          except
-          select distinct parent_id from sample_hierarchy) c
-    join sample_hierarchy h on c.sample_id = h.sample_id
-union all
-    select a.leaf_id, h.sample_id, h.parent_id
-    from sample_ancestors a
-    join sample_hierarchy h on a.parent_id = h.sample_id
-)
-
--- tuples of sample and one of its ancestors
--- root samples do not appear in the results
-, sample_ancestor_ids (sample_id, ancestor_id) as (
-    select leaf_id as sample_id, child_id as ancestor_id
-    from sample_ancestors
-    where leaf_id <> child_id and parent_id is not null
-)
-
-
--- paths from study to sample
-, sample_id_path as (
-    select e.study_id, s.experiment_id, s.sample_id
-    from experiment e
+with recursive study_samples (study_id, experiment_id, sample_id) as (
+    select st.study_id
+         , e.experiment_id
+         , s.sample_id
+    from study st
+    join experiment e on e.study_id = st.study_id
     join sample s on s.experiment_id = e.experiment_id
+    where st.sw_accession = ?
 )
 
--- paths from sample directly to file
-, sample_file_id_path as (
-    select ps.sample_id, ps.processing_id, pf.file_id
-    from processing_files pf
-    join processing_samples ps on pf.processing_id = ps.processing_id
+, study_processings (study_id, experiment_id, sample_id, sequencer_run_id, lane_id, ius_id, processing_id) as (
+    select ss.study_id
+         , ss.experiment_id
+         , ss.sample_id
+         , null as sequencer_run_id
+         , null as lane_id
+         , null as ius_id
+         , ps.processing_id
+    from processing_samples ps
+    join study_samples ss on ss.sample_id = ps.sample_id
+union
+    select ss.study_id
+         , ss.experiment_id
+         , ss.sample_id
+         , sr.sequencer_run_id
+         , l.lane_id
+         , i.ius_id
+         , pi.processing_id
+    from processing_ius pi
+    join ius i on i.ius_id = pi.ius_id
+    join lane l on l.lane_id = i.lane_id
+    join sequencer_run sr on sr.sequencer_run_id = l.sequencer_run_id
+    join study_samples ss on ss.sample_id = i.sample_id
+union
+    select sp.study_id
+         , sp.experiment_id
+         , sp.sample_id
+         , sp.sequencer_run_id
+         , sp.lane_id
+         , sp.ius_id
+         , pr.child_id as processing_id
+    from study_processings sp
+    join processing_relationship pr on pr.parent_id = sp.processing_id
 )
 
--- paths from sample to file via lane
-, lane_file_id_path as (
-    select l.sample_id, l.lane_id, pl.processing_id, pf.file_id
-    from processing_files pf
-    join processing_lanes pl on pf.processing_id = pl.processing_id
-    join lane l on pl.lane_id = l.lane_id
+, study_report_ids (study_id, experiment_id, sample_id, sequencer_run_id, lane_id, ius_id, processing_id, file_id) as (
+    select sp.study_id
+         , sp.experiment_id
+         , sp.sample_id
+         , sp.sequencer_run_id
+         , sp.lane_id
+         , sp.ius_id
+         , sp.processing_id
+         , pf.file_id
+    from study_processings sp
+    join processing_files pf on pf.processing_id = sp.processing_id
 )
 
--- paths from sample to file via ius
-, ius_file_id_path as (
-    select i.sample_id, i.ius_id, pi.processing_id, pf.file_id
-    from processing_files pf
-    join processing_ius pi on pf.processing_id = pi.processing_id
-    join ius i on pi.ius_id = i.ius_id
+, study_report_wf_ids (study_id, experiment_id, sample_id, sequencer_run_id, lane_id, ius_id, workflow_id, workflow_run_id, processing_id, file_id) as (
+    select so.study_id
+         , so.experiment_id
+         , so.sample_id
+         , so.sequencer_run_id
+         , so.lane_id
+         , so.ius_id
+         , wr.workflow_id
+         , wr.workflow_run_id
+         , so.processing_id
+         , so.file_id
+    from study_report_ids so
+    join processing p on p.processing_id = so.processing_id
+    left join workflow_run wr on (p.workflow_run_id is not null and wr.workflow_run_id = p.workflow_run_id)
+                              or (p.workflow_run_id is null and wr.workflow_run_id = p.ancestor_workflow_run_id)
 )
 
--- paths from study to file
-, file_id_path as (
-    select sp.study_id, sp.experiment_id, fp.*
-    from  (
-        select sample_id, lane_id, null as ius_id, processing_id, file_id from lane_file_id_path
-        union all
-        select sample_id, null as lane_id, ius_id, processing_id, file_id from ius_file_id_path
-        union all
-        select sample_id, null as lane_id, null as ius_id, processing_id, file_id from sample_file_id_path) fp
-    join sample_id_path sp on sp.sample_id =  fp.sample_id
+, sample_ancestors (sample_id, ancestor_id) as (
+    select sample_id
+         , parent_id as ancestor_id
+    from sample_hierarchy
+    where parent_id is not null
+union all
+    select sa.sample_id
+         , sh.parent_id as ancestor_id
+    from sample_ancestors sa
+    join sample_hierarchy sh on sh.sample_id = sa.ancestor_id
+    where parent_id is not null
 )
 
--- concatenated sample/tag values
 , study_attrs as (
     select study_id, tag, array_to_string(array_agg(value), ';') as vals
     from study_attribute sta
@@ -72,7 +93,7 @@ union all
     group by study_id, tag
 )
 
--- concatenated sample attributes
+
 , study_attrs_str as (
     select study_id
          , array_to_string(array_agg('study.'||tag||'='||vals), ';') as attrs
@@ -171,64 +192,64 @@ union all
          , array_to_string(array_agg(name), ':') as parent_names
          , array_to_string(array_agg(sw_accession), ':') as parent_swas
          , array_to_string(array_agg('parent_sample.'||tag||'.'||sw_accession||'='||vals), ';') as parent_attrs
-    from sample_ancestor_ids anc
+    from sample_ancestors anc
     join sample_attrs attr on attr.sample_id = anc.ancestor_id
     join sample s on s.sample_id = anc.ancestor_id
     group by anc.sample_id
 )
 
-
-select p.update_tstmp as processing_date
+select p.update_tstmp as last_modified
      , translate(st.title, ' ', '_') as study_title
      , st.sw_accession as study_swa
-     , case when sta.attrs is not null then sta.attrs else '' end as study_attrs
+     , sta.attrs as study_attrs
      , translate(case when e.name is not null and e.name <> '' then e.name else e.title end , ' ', '_') as experiment_name
      , e.sw_accession as experiment_swa
-     , case when ea.attrs is not null then ea.attrs else '' end as experiment_attrs
+     , ea.attrs as experiment_attrs
      , translate(sp.parent_names, ' ', '_') as sample_parent_names
      , sp.parent_swas as sample_parent_swas
      , sp.parent_attrs as sample_parent_attrs
      , translate(case when s.name is not null and s.name <> '' then s.name else s.title end , ' ', '_') as sample_name
      , s.sw_accession as sample_swa
-     , case when sa.attrs is not null then sa.attrs else '' end as sample_attrs
+     , sa.attrs as sample_attrs
      , translate(sr.name, ' ', '_') as sequencer_run_name
      , sr.sw_accession as sequencer_run_swa
-     , case when sra.attrs is not null then sra.attrs else '' end as sequencer_run_attrs
+     , sra.attrs as sequencer_run_attrs
      , translate(l.name, ' ', '_') as lane_name
-     , l.lane_index as lane_index
+     , l.lane_index+1 as lane_number
      , l.sw_accession as lane_swa
-     , case when la.attrs is not null then la.attrs else '' end as lane_attrs
+     , la.attrs as lane_attrs
      , i.tag as ius_tag
      , i.sw_accession as ius_swa
-     , case when ia.attrs is not null then ia.attrs else '' end as ius_attrs
+     , ia.attrs as ius_attrs
      , translate(wf.name, ' ', '_') as workflow_name
      , wf.version as workflow_version
      , wf.sw_accession as workflow_swa
      , translate(wfr.name, ' ', '_') as workflow_run_name
---     , wfr.status as workflow_run_status
+     , wfr.status as workflow_run_status
      , wfr.sw_accession as workflow_run_swa
      , p.algorithm as processing_algorithm
      , p.sw_accession as processing_swa
-     , case when pa.attrs is not null then pa.attrs else '' end as processing_attrs
+     , pa.attrs as processing_attrs
      , f.meta_type as file_meta_type
      , f.sw_accession as file_swa
      , f.file_path as file_path
-from file_id_path fp
-join file f on f.file_id = fp.file_id
-join study st on st.study_id = fp.study_id
-left join study_attrs_str sta on sta.study_id = fp.study_id
-join experiment e on e.experiment_id = fp.experiment_id
-left join experiment_attrs_str ea on ea.experiment_id = fp.experiment_id
-join processing p on p.processing_id = fp.processing_id
-left join processing_attrs_str pa on pa.processing_id = fp.processing_id
-left join sample_parent_strs sp on sp.sample_id = fp.sample_id
-left join sample s on s.sample_id = fp.sample_id
-left join sample_attrs_str sa on sa.sample_id = fp.sample_id
-left join lane l on l.lane_id = fp.lane_id
-left join lane_attrs_str la on la.lane_id = fp.lane_id
-left join sequencer_run sr on sr.sequencer_run_id = l.sequencer_run_id
-left join sequencer_run_attrs_str sra on sra.sequencer_run_id = sr.sequencer_run_id
-left join ius i on i.ius_id = fp.ius_id
-left join ius_attrs_str ia on ia.ius_id = fp.ius_id
-left join workflow_run wfr on wfr.workflow_run_id = p.workflow_run_id
+from study_report_ids ids
+join file f on f.file_id = ids.file_id
+join study st on st.study_id = ids.study_id
+left join study_attrs_str sta on sta.study_id = ids.study_id
+join experiment e on e.experiment_id = ids.experiment_id
+left join experiment_attrs_str ea on ea.experiment_id = ids.experiment_id
+join processing p on p.processing_id = ids.processing_id
+left join processing_attrs_str pa on pa.processing_id = ids.processing_id
+left join sample_parent_strs sp on sp.sample_id = ids.sample_id
+left join sample s on s.sample_id = ids.sample_id
+left join sample_attrs_str sa on sa.sample_id = ids.sample_id
+left join lane l on l.lane_id = ids.lane_id
+left join lane_attrs_str la on la.lane_id = ids.lane_id
+left join sequencer_run sr on sr.sequencer_run_id = ids.sequencer_run_id
+left join sequencer_run_attrs_str sra on sra.sequencer_run_id = ids.sequencer_run_id
+left join ius i on i.ius_id = ids.ius_id
+left join ius_attrs_str ia on ia.ius_id = ids.ius_id
+left join workflow_run wfr on (p.workflow_run_id is not null and wfr.workflow_run_id = p.workflow_run_id)
+                           or (p.workflow_run_id is null and wfr.workflow_run_id = p.ancestor_workflow_run_id)
 left join workflow wf on wf.workflow_id = wfr.workflow_id
