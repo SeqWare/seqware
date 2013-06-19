ad9a8abc14dd844fabe9a7baeb291a01103bb7d0
Denis Yuen <denis.yuen@oicr.on.ca>
2013-06-19 14:15:53 -0400

    Allow some extended tests that do not require Pegasus/Globus/Condor to execute normally.
    Former-commit-id: 49d0a0f066455f386461b68018bd3dc96b29fb8d

--- seqware-ext-testing/src/test/java/net/sourceforge/seqware/pipeline/plugins/AttributeAnnotatorIT.java
+++ seqware-ext-testing/src/test/java/net/sourceforge/seqware/pipeline/plugins/AttributeAnnotatorIT.java
@@ -16,18 +16,8 @@
  */
 package net.sourceforge.seqware.pipeline.plugins;
 
-import com.google.common.io.Files;
-import java.io.File;
-import java.io.IOException;
-import java.util.List;
-import java.util.UUID;
-import junit.framework.Assert;
-import net.sourceforge.seqware.common.module.ReturnValue;
-import net.sourceforge.seqware.common.util.Log;
 import net.sourceforge.seqware.pipeline.modules.utilities.ExternalTestingUtility;
-import org.apache.commons.io.FileUtils;
 import org.junit.BeforeClass;
-import org.junit.Test;
 
 /**
  * These tests support command-line tools found in the SeqWare User Tutorial,
