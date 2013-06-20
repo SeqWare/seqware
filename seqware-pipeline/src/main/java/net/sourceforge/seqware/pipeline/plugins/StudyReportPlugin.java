e35af54e08163dbf75267ccfa5b747a244f0d13a
Alexander Taggart <Alexander.Taggart@oicr.on.ca>
2013-06-20 15:54:52 -0400

    Use default study report output file name instead of writing to stdout.
    Former-commit-id: 04ab7f8982c40715ed25d0d16aa0c9d078db602e

--- seqware-pipeline/src/main/java/net/sourceforge/seqware/pipeline/plugins/StudyReportPlugin.java
+++ seqware-pipeline/src/main/java/net/sourceforge/seqware/pipeline/plugins/StudyReportPlugin.java
@@ -1,10 +1,12 @@
 package net.sourceforge.seqware.pipeline.plugins;
 
 import java.io.BufferedWriter;
+import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.io.OutputStreamWriter;
 import java.io.Writer;
+import java.util.Date;
 
 import net.sourceforge.seqware.common.module.ReturnValue;
 import net.sourceforge.seqware.pipeline.plugin.Plugin;
@@ -17,7 +19,8 @@ public class StudyReportPlugin extends Plugin {
 
   public StudyReportPlugin() {
     parser.accepts("all", "Generate a report of all studies. Or use '--title'.");
-    parser.accepts("title", "The title of the study whose report will be generated. Or use '--all'.").withRequiredArg();
+    parser.accepts("title",
+                   "The title of the study whose report will be generated. Or use '--all'.").withRequiredArg();
     parser.accepts("out", "The file into which the report will be written.").withRequiredArg();
   }
 
@@ -35,31 +38,29 @@ public class StudyReportPlugin extends Plugin {
   public ReturnValue do_run() {
     if (options.has("title")) {
       String title = (String) options.valueOf("title");
-      metadata.studyReport(title, writer());
+      metadata.studyReport(title, writer(title));
       return new ReturnValue();
     } else if (options.has("all")) {
-      metadata.allStudiesReport(writer());
+      metadata.allStudiesReport(writer("all"));
       return new ReturnValue();
-    }
-    else {
+    } else {
       println("Missing 'title' parameter");
       println(this.get_syntax());
       return new ReturnValue(ReturnValue.INVALIDPARAMETERS);
     }
   }
-  
-  private Writer writer(){
-    Writer out;
-    if (options.has("out")) {
-      try {
-        out = new BufferedWriter(new FileWriter((String) options.valueOf("out")));
-      } catch (IOException e) {
-        throw new RuntimeException(e);
+
+  private Writer writer(String label) {
+    try {
+      if (options.has("out")) {
+        return new BufferedWriter(new FileWriter((String) options.valueOf("out")));
+      } else {
+        String filename = (new Date() + "__" + label+".tsv").replace(" ", "_");
+        return new BufferedWriter(new FileWriter(filename));
       }
-    } else {
-      out = new OutputStreamWriter(System.out);
+    } catch (IOException e) {
+      throw new RuntimeException(e);
     }
-    return out;
   }
 
   @Override
