e10475b96db77f3f750af492343e84c48a6e3168
Alexander Taggart <Alexander.Taggart@oicr.on.ca>
2013-07-02 14:45:59 -0400

    Plumb slots and maxmem qsub param format strings through the onion.

--- seqware-pipeline/src/main/java/net/sourceforge/seqware/pipeline/workflowV2/engine/oozie/object/OoziePerlJob.java
+++ seqware-pipeline/src/main/java/net/sourceforge/seqware/pipeline/workflowV2/engine/oozie/object/OoziePerlJob.java
@@ -6,9 +6,9 @@ import net.sourceforge.seqware.pipeline.workflowV2.model.AbstractJob;
 
 public class OoziePerlJob extends OozieJob {
 
-  public OoziePerlJob(AbstractJob job, String name, String oozie_working_dir,
-                      boolean useSge, File seqwareJar) {
-    super(job, name, oozie_working_dir, useSge, seqwareJar);
+  public OoziePerlJob(AbstractJob job, String name, String oozie_working_dir, boolean useSge, File seqwareJar,
+                      String slotsSgeParamFormat, String maxMemorySgeParamFormat) {
+    super(job, name, oozie_working_dir, useSge, seqwareJar, slotsSgeParamFormat, maxMemorySgeParamFormat);
 
   }
 
