a86736f422a3106ec2ead4119e7b89460ced11ad
Alexander Taggart <Alexander.Taggart@oicr.on.ca>
2013-08-14 16:25:19 -0400

    Do not reply on seqware built files from the host machine.
    Former-commit-id: 786b55b8582adbf845d95227e63d6137cee42bcd

--- seqware-vagrant/templates/server_setup_scripts/ubuntu_12.04_master_script.sh
+++ seqware-vagrant/templates/server_setup_scripts/ubuntu_12.04_master_script.sh
@@ -128,8 +128,10 @@ su - seqware -c 'cd /home/seqware/gitroot/seqware; git hf init; git hf update'
 su - seqware -c 'cd /home/seqware/gitroot/seqware; %{SEQWARE_BRANCH_CMD}'
 su - seqware -c 'cd /home/seqware/gitroot/seqware; %{SEQWARE_BUILD_CMD} 2>&1 | tee build.log'
 
+export SEQWARE_VERSION=`ls /home/seqware/gitroot/seqware/seqware-distribution/target/seqware-distribution-*-full.jar | grep -Eo '[0-9]+\.[0-9]+\.[0-9]+(-SNAPSHOT)?' | head -1`
+
 # setup jar
-cp /home/seqware/gitroot/seqware/seqware-distribution/target/seqware-distribution-%{SEQWARE_VERSION}-full.jar /home/seqware/jars/
+cp /home/seqware/gitroot/seqware/seqware-distribution/target/seqware-distribution-${SEQWARE_VERSION}-full.jar /home/seqware/jars/
 
 # setup seqware cli
 cp /home/seqware/gitroot/seqware/seqware-pipeline/target/seqware /home/seqware/bin
@@ -169,18 +171,20 @@ sudo -u postgres psql test_seqware_meta_db < /tmp/seqware_meta_db_data.sql
 rm -rf /var/lib/tomcat6/webapps/ROOT
 
 # seqware web service
-cp /home/seqware/gitroot/seqware/seqware-webservice/target/seqware-webservice-%{SEQWARE_VERSION}.war /var/lib/tomcat6/webapps/SeqWareWebService.war
-mv /vagrant/seqware-webservice-%{SEQWARE_VERSION}.xml /etc/tomcat6/Catalina/localhost/SeqWareWebService.xml
+cp /home/seqware/gitroot/seqware/seqware-webservice/target/seqware-webservice-${SEQWARE_VERSION}.war /var/lib/tomcat6/webapps/SeqWareWebService.war
+cp /home/seqware/gitroot/seqware/seqware-webservice/target/seqware-webservice-${SEQWARE_VERSION}.xml /etc/tomcat6/Catalina/localhost/SeqWareWebService.xml
+perl -pi -e "s/test_seqware_meta_db/seqware_meta_db/;" /etc/tomcat6/Catalina/localhost/SeqWareWebService.xml
 
 # seqware portal
-cp /home/seqware/gitroot/seqware/seqware-portal/target/seqware-portal-%{SEQWARE_VERSION}.war /var/lib/tomcat6/webapps/SeqWarePortal.war
-mv /vagrant/seqware-portal-%{SEQWARE_VERSION}.xml /etc/tomcat6/Catalina/localhost/SeqWarePortal.xml
+cp /home/seqware/gitroot/seqware/seqware-portal/target/seqware-portal-${SEQWARE_VERSION}.war /var/lib/tomcat6/webapps/SeqWarePortal.war
+cp /home/seqware/gitroot/seqware/seqware-portal/target/seqware-portal-${SEQWARE_VERSION}.xml /etc/tomcat6/Catalina/localhost/SeqWarePortal.xml
+perl -pi -e "s/test_seqware_meta_db/seqware_meta_db/;" /etc/tomcat6/Catalina/localhost/SeqWarePortal.xml
 
 # restart tomcat6
 /etc/init.d/tomcat6 start
 
 # seqware landing page
-mv /vagrant/vm_landing/* /var/www/
+cp -r /home/seqware/gitroot/seqware/seqware-distribution/docs/vm_landing/* /var/www/
 
 # seqware tutorials
 sudo mkdir /datastore
