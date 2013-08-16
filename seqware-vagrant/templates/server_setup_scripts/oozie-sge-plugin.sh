4c300fbaefe19252bdb765ba54cf8a18e437cd4e
Alexander Taggart <Alexander.Taggart@oicr.on.ca>
2013-08-16 10:41:04 -0400

    Fix oozie-sge install, add helloworld workflow install
    Former-commit-id: fcc9c1006ec7bc56739d6f878a43f67ce2fdb692

--- seqware-vagrant/templates/server_setup_scripts/oozie-sge-plugin.sh
+++ seqware-vagrant/templates/server_setup_scripts/oozie-sge-plugin.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/env bash -vx
+#!/bin/bash -vx
 
 OOZIE_HOME_DIR=`ps -fww -u oozie | grep -oE '\-Doozie\.home\.dir=\S+' | grep -oE '/\S+'`
 OOZIE_CONF_DIR=`ps -fww -u oozie | grep -oE '\-Doozie\.config\.dir=\S+' | grep -oE '/\S+'`
@@ -10,51 +10,57 @@ OOZIE_CONF_DIR=`ps -fww -u oozie | grep -oE '\-Doozie\.config\.dir=\S+' | grep -
 # OOZIE_ACTION_RECHECK_PERIOD=...
 
 # Set to true to emit debug logging of the oozie-sge plugin
-OOZIE_SGE_DEBUG_LOG=${OOZIE_SGE_DEBUG_LOG:=false}
+OOZIE_SGE_DEBUG_LOG=${OOZIE_SGE_DEBUG_LOG:=true}
 
 
 if [ ! -z "$OOZIE_HOME_DIR" ]; then
 
     if [ ! -z "$OOZIE_CONF_DIR" ]; then
 
-        OOZIE_WEBAPP_LIB_DIR=$OOZIE_HOME_DIR/webapps/oozie/WEB-INF/lib
-        mkdir -p $OOZIE_WEBAPP_LIB_DIR
-        cd $OOZIE_WEBAPP_LIB_DIR
-
-        if type -p curl >/dev/null 2>&1; then
-            HTTP_CLIENT="curl $CURL_PROXY -f -L -O"
-        else
-            HTTP_CLIENT="wget"
-        fi
-
-        $HTTP_CLIENT "http://wrench.res.oicr.on.ca/artifactory/seqware-dependencies/io/seqware/oozie-sge/1.0.0/oozie-sge-1.0.0.jar"
-        $HTTP_CLIENT "http://repo1.maven.org/maven2/org/apache/commons/commons-exec/1.1/commons-exec-1.1.jar"
-        chmod +x *.jar
-
-        cd $OOZIE_CONF_DIR
-        perl -pi -e "s/org.apache.oozie.action.email.EmailActionExecutor/io.seqware.oozie.action.sge.SgeActionExecutor,org.apache.oozie.action.email.EmailActionExecutor/;" oozie-site.xml
-        perl -pi -e "s/shell-action-/sge-action-1.0.xsd,shell-action-/;" oozie-site.xml
-
-        if [ ! -z "$OOZIE_ACTION_RECHECK_PERIOD" ]; then
-            perl -pi -e  "s/<configuration>/<configuration>\n<property><name>oozie.service.ActionCheckerService.action.check.delay<\/name><value>${OOZIE_ACTION_RECHECK_PERIOD}<\/value><\/property>/;" oozie-site.xml
-        fi
-
-        if [ ! -z "$OOZIE_CHECK_SERVICE_PERIOD" ]; then
-            perl -pi -e  "s/<configuration>/<configuration>\n<property><name>oozie.service.ActionCheckerService.action.check.interval<\/name><value>${OOZIE_CHECK_SERVICE_PERIOD}<\/value><\/property>/;" oozie-site.xml
-        fi
-
-        if $OOZIE_SGE_DEBUG_LOG; then
-            echo "log4j.logger.io.seqware.oozie.action.sge=DEBUG, oozie" >> $OOZIE_CONF_DIR/oozie-log4j.properties
-        fi
-
         QSUB=`which qsub`
 
-        echo '# Allow oozie user to qsub as other users:' >> /etc/sudoers
-        echo "oozie ALL=(ALL) NOPASSWD: $QSUB" >> /etc/sudoers
+        if [ ! -z "$QSUB" ]; then
 
-        /etc/init.d/oozie restart
+            OOZIE_WEBAPP_LIB_DIR=$OOZIE_HOME_DIR/webapps/oozie/WEB-INF/lib
+            mkdir -p $OOZIE_WEBAPP_LIB_DIR
+            cd $OOZIE_WEBAPP_LIB_DIR
 
-    else
+            if type -p curl >/dev/null 2>&1; then
+                HTTP_CLIENT="curl $CURL_PROXY -f -L -O"
+            else
+                HTTP_CLIENT="wget"
+            fi
+
+            $HTTP_CLIENT "http://wrench.res.oicr.on.ca/artifactory/seqware-dependencies/io/seqware/oozie-sge/1.0.0/oozie-sge-1.0.0.jar"
+            $HTTP_CLIENT "http://repo1.maven.org/maven2/org/apache/commons/commons-exec/1.1/commons-exec-1.1.jar"
+            chmod +x *.jar
+
+            cd $OOZIE_CONF_DIR
+            perl -pi -e "s/org.apache.oozie.action.email.EmailActionExecutor/io.seqware.oozie.action.sge.SgeActionExecutor,org.apache.oozie.action.email.EmailActionExecutor/;" oozie-site.xml
+            perl -pi -e "s/shell-action-/sge-action-1.0.xsd,shell-action-/;" oozie-site.xml
+
+            if [ ! -z "$OOZIE_ACTION_RECHECK_PERIOD" ]; then
+                perl -pi -e  "s/<configuration>/<configuration>\n<property><name>oozie.service.ActionCheckerService.action.check.delay<\/name><value>${OOZIE_ACTION_RECHECK_PERIOD}<\/value><\/property>/;" oozie-site.xml
+            fi
+
+            if [ ! -z "$OOZIE_CHECK_SERVICE_PERIOD" ]; then
+                perl -pi -e  "s/<configuration>/<configuration>\n<property><name>oozie.service.ActionCheckerService.action.check.interval<\/name><value>${OOZIE_CHECK_SERVICE_PERIOD}<\/value><\/property>/;" oozie-site.xml
+            fi
+
+            if $OOZIE_SGE_DEBUG_LOG; then
+                echo "log4j.logger.io.seqware.oozie.action.sge=DEBUG, oozie" >> $OOZIE_CONF_DIR/oozie-log4j.properties
+            fi
+
+            echo '# Allow oozie user to qsub as other users:' >> /etc/sudoers
+            echo "oozie ALL=(ALL) NOPASSWD: $QSUB" >> /etc/sudoers
+
+            /etc/init.d/oozie restart
+
+        else
+            echo "Path to qsub could not be resolved. Terminating."
+            exit 1
+        fi
+     else
         echo "Oozie conf directory could not be resolved. Terminating."
         exit 1
     fi
