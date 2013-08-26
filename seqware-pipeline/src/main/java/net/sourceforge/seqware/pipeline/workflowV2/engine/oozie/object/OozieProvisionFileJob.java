f682fc3e11b0fed9610dd15d57ca893131ce5de6
Alexander Taggart <Alexander.Taggart@oicr.on.ca>
2013-08-26 17:39:19 -0400

    Emit SEQWARE_SETTINGS into Oozie java jobs.

--- seqware-pipeline/src/main/java/net/sourceforge/seqware/pipeline/workflowV2/engine/oozie/object/OozieProvisionFileJob.java
+++ seqware-pipeline/src/main/java/net/sourceforge/seqware/pipeline/workflowV2/engine/oozie/object/OozieProvisionFileJob.java
@@ -4,6 +4,7 @@ import java.io.File;
 import java.util.ArrayList;
 import java.util.List;
 
+import net.sourceforge.seqware.common.util.configtools.ConfigTools;
 import net.sourceforge.seqware.pipeline.workflowV2.model.AbstractJob;
 import net.sourceforge.seqware.pipeline.workflowV2.model.SqwFile;
 
@@ -43,6 +44,7 @@ public class OozieProvisionFileJob extends OozieJob {
     addProp(config, "oozie.launcher.mapred.job.reduce.memory.mb", jobObj.getMaxMemory());
     addProp(config, "oozie.launcher.mapreduce.map.memory.physical.mb", jobObj.getMaxMemory());
     addProp(config, "oozie.launcher.mapreduce.reduce.memory.physical.mb", jobObj.getMaxMemory());
+    addProp(config, ConfigTools.SEQWARE_SETTINGS_PROPERTY, ConfigTools.getSettingsFilePath());
 
     add(java, "main-class", "net.sourceforge.seqware.pipeline.runner.Runner");
     for (String arg : runnerArgs()) {
