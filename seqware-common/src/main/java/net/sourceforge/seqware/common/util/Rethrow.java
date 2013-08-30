4c7d46ad14b74841d12dd39287a9ec7515d9c718
Alexander Taggart <Alexander.Taggart@oicr.on.ca>
2013-08-30 12:35:16 -0400

    Add docs.
    Former-commit-id: 3f069d57a1421ab44f0b0c6972e9c401d36213b4

--- seqware-common/src/main/java/net/sourceforge/seqware/common/util/Rethrow.java
+++ seqware-common/src/main/java/net/sourceforge/seqware/common/util/Rethrow.java
@@ -2,6 +2,24 @@ package net.sourceforge.seqware.common.util;
 
 public class Rethrow {
 
+  /**
+   * <p>
+   * Rethrows <tt>t</tt> if unchecked, otherwise throws <tt>t</tt> wrapped in a
+   * RuntimeException.
+   * 
+   * <p>
+   * Use pattern:
+   * 
+   * <pre>
+   * try {
+   *   foobar();
+   * } catch (Exception e) {
+   *   rethrow(e); // import static
+   * }
+   * </pre>
+   * 
+   * @param t
+   */
   public static void rethrow(Throwable t) {
     if (t instanceof RuntimeException)
       throw (RuntimeException) t;
--- seqware-pipeline/src/main/java/net/sourceforge/seqware/pipeline/tools/RunLock.java
+++ seqware-pipeline/src/main/java/net/sourceforge/seqware/pipeline/tools/RunLock.java
@@ -5,8 +5,26 @@ import it.sauronsoftware.junique.AlreadyLockedException;
 import it.sauronsoftware.junique.JUnique;
 import net.sourceforge.seqware.common.util.configtools.ConfigTools;
 
+/**
+ * <p>
+ * Allows for preventing concurrent processes <em>owned by the same user</em>.
+ * 
+ * <p>
+ * By default the lock identifier is a constant. If more flexibility is needed,
+ * provide a value to <tt>SW_LOCK_ID</tt> in the seqware settings file.
+ * 
+ * <p>
+ * WARNING: This system will fail to prevent concurrent processes if the value
+ * of the SW_LOCK_ID is changed/added/removed in the time between multiple
+ * processes acquiring a lock.
+ * 
+ */
 public class RunLock {
 
+  /**
+   * Acquires a lock or throws an unchecked exception if it could not be
+   * acquired.
+   */
   public static void acquire() {
     try {
       JUnique.acquireLock(id());
@@ -15,6 +33,9 @@ public class RunLock {
     }
   }
 
+  /**
+   * Releases a lock if one was acquired, otherwise no-op.
+   */
   public static void release() {
     JUnique.releaseLock(id());
   }
