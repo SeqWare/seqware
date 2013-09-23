ab47a4f592a0c3fd9a589936b07690e43973b9e6
Denis Yuen <denis.yuen@oicr.on.ca>
2013-09-23 18:25:38 -0400

    Fix stupid byte-order mark issue and some progress on workflow run conventions checking
    Former-commit-id: f0bef73913fa268b5b38c18386562ae6939d69d9

---
+++ seqware-pipeline/src/main/resources/net/sourceforge/seqware/pipeline/plugins/checkdb/plugins/new_input_files_versus_old.sql
@@ -0,0 +1,17 @@
+WITH new_input_files AS 
+(select wr.sw_accession AS workflow_run_swa, f.* from workflow_run wr, workflow_run_input_files wrif, file f
+WHERE wr.workflow_run_id = wrif.workflow_run_id
+AND wrif.file_id = f.file_id 
+ORDER BY wr.sw_accession)
+,
+old_input_files AS 
+(select wr.sw_accession AS workflow_run_swa, f.* from workflow_run wr, processing p1, 
+processing_relationship pr, processing p2, processing_files pf, file f
+WHERE wr.workflow_run_id = p1.workflow_run_id 
+AND p1.processing_id = pr.child_id 
+AND pr.parent_id = p2.processing_id 
+AND p2.processing_id = pf.processing_id 
+AND pf.file_id = f.file_id  
+ORDER BY wr.sw_accession)
+
+SELECT * from new_input_files except select * from old_input_files;
