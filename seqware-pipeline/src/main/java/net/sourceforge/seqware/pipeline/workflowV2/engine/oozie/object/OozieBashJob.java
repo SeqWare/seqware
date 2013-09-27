e484335bd7504b1bb65c13fc9e3a0ebf9cf6290c
Alexander Taggart <Alexander.Taggart@oicr.on.ca>
2013-09-27 15:04:39 -0400

    Use correct xml node.
    Former-commit-id: ffa9f9c09e3553e505a879fa698680b52a03af90

--- seqware-pipeline/src/main/java/net/sourceforge/seqware/pipeline/workflowV2/engine/oozie/object/OozieBashJob.java
+++ seqware-pipeline/src/main/java/net/sourceforge/seqware/pipeline/workflowV2/engine/oozie/object/OozieBashJob.java
@@ -46,7 +46,7 @@ public class OozieBashJob extends OozieJob {
 
     add(java, "main-class", "net.sourceforge.seqware.pipeline.runner.Runner");
     String settings = String.format("-D%s='%s'", ConfigTools.SEQWARE_SETTINGS_PROPERTY, ConfigTools.getSettingsFilePath());
-    add(java, "arg", settings);
+    add(java, "java-opts", settings);
     for (String arg : runnerArgs(jobScript)) {
       add(java, "arg", arg);
     }
