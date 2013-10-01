dca865f7f74321cded147da6b82ec6b1ed9ff7c7
Denis Yuen <denis.yuen@oicr.on.ca>
2013-10-01 10:25:39 -0400

    Remove query engine code to reduce VM provision, compile, and test time
    QueryEngine is now in https://github.com/SeqWare/queryengine
    
    
    Former-commit-id: 7b8dd000535de1276e80c7e39cb75e73373aeeba

--- seqware-queryengine/src/main/java/com/github/seqware/queryengine/model/impl/inMemory/package-info.java
+++
@@ -1,5 +0,0 @@
-/**
- * Provides classes that support a simple in-memory implementation of the various
- * model classes. 
- */
-package com.github.seqware.queryengine.model.impl.inMemory;
--- seqware-queryengine/src/main/java/com/github/seqware/queryengine/system/exporters/package-info.java
+++
@@ -1,4 +0,0 @@
-/**
- * Command-line tools for exporting files to external file types. 
- */
-package com.github.seqware.queryengine.system.exporters;
