36c2732092a5587e50fccd0ed4fd099ab1c0ee81
Denis Yuen <denis.yuen@oicr.on.ca>
2013-10-17 18:13:33 -0400

    Initial prototype of recursive deletion
    Used container managed JTA transactions, JSON file indicates files to be deleted, and is sent back in order to guide the deletion.
    Need to decide how to handle processing_relationship as a design decision.
    
    
    Former-commit-id: aa3690c60c530d91a5e5723b0eb73b6d4ba6b53b

--- seqware-admin-webservice/src/main/java/io/seqware/webservice/controller/WorkflowRunFacadeREST.java
+++ seqware-admin-webservice/src/main/java/io/seqware/webservice/controller/WorkflowRunFacadeREST.java
@@ -4,11 +4,29 @@
  */
 package io.seqware.webservice.controller;
 
+import io.seqware.webservice.model.File;
+import io.seqware.webservice.model.Processing;
+import io.seqware.webservice.model.ProcessingFiles;
 import io.seqware.webservice.model.WorkflowRun;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
+import javax.annotation.Resource;
 import javax.ejb.Stateless;
+import javax.ejb.TransactionAttribute;
+import javax.ejb.TransactionAttributeType;
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
+import javax.transaction.HeuristicMixedException;
+import javax.transaction.HeuristicRollbackException;
+import javax.transaction.NotSupportedException;
+import javax.transaction.RollbackException;
+import javax.transaction.SystemException;
+import javax.transaction.UserTransaction;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.DELETE;
 import javax.ws.rs.GET;
@@ -85,5 +103,60 @@ public class WorkflowRunFacadeREST extends AbstractFacade<WorkflowRun> {
   protected EntityManager getEntityManager() {
     return em;
   }
+  
+  @TransactionAttribute(TransactionAttributeType.REQUIRES_NEW)
+  @DELETE
+  @Path("{id}/rdelete")
+  @Consumes({"application/json"})
+  public void deleteRecursive(@PathParam("id") Integer id, List<ModelAccessionIDTuple> victims) throws NamingException, NotSupportedException, SystemException, RollbackException, HeuristicMixedException, HeuristicRollbackException, ClassNotFoundException {
+      EntityManager entityManager = getEntityManager();
+      for(ModelAccessionIDTuple t : victims){
+          Object found = entityManager.find(Class.forName(t.getAdminModelClass()), t.getId());
+          entityManager.remove(found);
+      }
+      WorkflowRun find = entityManager.find(WorkflowRun.class, id);
+      entityManager.remove(find);
+  }
+
+  
+  /**
+   * Returns the list of potential victims for a deletion operation
+   * @param id
+   * @return 
+   */
+    @GET
+    @Path("{id}/rdelete")
+    @Produces({"application/json"})
+    public List<ModelAccessionIDTuple> findRecursive(@PathParam("id") Integer id) {
+        List<ModelAccessionIDTuple> results = new ArrayList<ModelAccessionIDTuple>();
+        WorkflowRun data = getEntityManager().find(WorkflowRun.class, id);
+        Set<Processing> affectedProcessing = new HashSet<Processing>();
+        // workflow_run
+        if (data.getProcessingCollection() != null) {
+            affectedProcessing.addAll(data.getProcessingCollection());
+        }
+        // ancestor_workflow_run
+        if (data.getProcessingCollection1() != null) {
+            affectedProcessing.addAll(data.getProcessingCollection1());
+        }
+        Set<File> affectedFile = new HashSet<File>();
+        for (Processing p : affectedProcessing) {
+            Collection<ProcessingFiles> processingFilesCollection = p.getProcessingFilesCollection();
+            if (processingFilesCollection == null) {
+                continue;
+            }
+            for (ProcessingFiles pf : processingFilesCollection) {
+                affectedFile.add(pf.getFileId());
+            }
+        }
+        // list all affected resources
+        for (Processing p : affectedProcessing) {
+            results.add(new ModelAccessionIDTuple(p.getSwAccession(), p.getProcessingId(), p.getClass().getName()));
+        }
+        for (File f : affectedFile) {
+            results.add(new ModelAccessionIDTuple(f.getSwAccession(), f.getFileId(), f.getClass().getName()));
+        }
+        return results;
+    }
   
 }
