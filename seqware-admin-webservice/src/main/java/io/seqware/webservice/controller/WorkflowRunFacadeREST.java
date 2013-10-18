bdc334d911bdc0df1fe4c13479b1f1a96f23cca2
Denis Yuen <denis.yuen@oicr.on.ca>
2013-10-18 12:17:06 -0400

    Working recursive delete starting from a workflow run and cascading down to child workflow runs.
    Former-commit-id: 95a73ebe54b9d446e2399056d492c6ff5f974d26

--- seqware-admin-webservice/src/main/java/io/seqware/webservice/controller/WorkflowRunFacadeREST.java
+++ seqware-admin-webservice/src/main/java/io/seqware/webservice/controller/WorkflowRunFacadeREST.java
@@ -7,17 +7,16 @@ package io.seqware.webservice.controller;
 import io.seqware.webservice.model.File;
 import io.seqware.webservice.model.Processing;
 import io.seqware.webservice.model.ProcessingFiles;
+import io.seqware.webservice.model.ProcessingRelationship;
 import io.seqware.webservice.model.WorkflowRun;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
-import javax.annotation.Resource;
 import javax.ejb.Stateless;
 import javax.ejb.TransactionAttribute;
 import javax.ejb.TransactionAttributeType;
-import javax.naming.InitialContext;
 import javax.naming.NamingException;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
@@ -26,7 +25,6 @@ import javax.transaction.HeuristicRollbackException;
 import javax.transaction.NotSupportedException;
 import javax.transaction.RollbackException;
 import javax.transaction.SystemException;
-import javax.transaction.UserTransaction;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.DELETE;
 import javax.ws.rs.GET;
@@ -104,18 +102,24 @@ public class WorkflowRunFacadeREST extends AbstractFacade<WorkflowRun> {
     return em;
   }
   
+  /**
+   * The actual delete method, container managed JTA transactions should handle rollback and atomic operations
+   * @param id
+   * @param victims
+   * @throws NamingException
+   * @throws NotSupportedException
+   * @throws SystemException
+   * @throws RollbackException
+   * @throws HeuristicMixedException
+   * @throws HeuristicRollbackException
+   * @throws ClassNotFoundException 
+   */
   @TransactionAttribute(TransactionAttributeType.REQUIRES_NEW)
   @DELETE
   @Path("{id}/rdelete")
   @Consumes({"application/json"})
-  public void deleteRecursive(@PathParam("id") Integer id, List<ModelAccessionIDTuple> victims) throws NamingException, NotSupportedException, SystemException, RollbackException, HeuristicMixedException, HeuristicRollbackException, ClassNotFoundException {
-      EntityManager entityManager = getEntityManager();
-      for(ModelAccessionIDTuple t : victims){
-          Object found = entityManager.find(Class.forName(t.getAdminModelClass()), t.getId());
-          entityManager.remove(found);
-      }
-      WorkflowRun find = entityManager.find(WorkflowRun.class, id);
-      entityManager.remove(find);
+  public void deleteRecursive(@PathParam("id") Integer id, Set<ModelAccessionIDTuple> victims) throws NamingException, NotSupportedException, SystemException, RollbackException, HeuristicMixedException, HeuristicRollbackException, ClassNotFoundException {
+      deleteWorkflowRunRecursive(id, true, victims);
   }
 
   
@@ -127,9 +131,27 @@ public class WorkflowRunFacadeREST extends AbstractFacade<WorkflowRun> {
     @GET
     @Path("{id}/rdelete")
     @Produces({"application/json"})
-    public List<ModelAccessionIDTuple> findRecursive(@PathParam("id") Integer id) {
-        List<ModelAccessionIDTuple> results = new ArrayList<ModelAccessionIDTuple>();
-        WorkflowRun data = getEntityManager().find(WorkflowRun.class, id);
+    public Set<ModelAccessionIDTuple> findRecursive(@PathParam("id") Integer id) {
+        return deleteWorkflowRunRecursive(id, false, null);
+    }
+
+    /**
+     * Method that starts from a workflow run, either deleting appropriate
+     * entities or reporting on what would have been deleted
+     *
+     * @param id primary key id for the workflow run
+     * @param delete whether to actually go through with deletion
+     * @param matchSet when actually doing deletion, this set should be a
+     * superset of what we find
+     * @return
+     */
+    protected Set<ModelAccessionIDTuple> deleteWorkflowRunRecursive(Integer id, boolean delete, Set<ModelAccessionIDTuple> matchSet) {
+        EntityManager entityManager = getEntityManager();
+        Set<ModelAccessionIDTuple> results = new HashSet<ModelAccessionIDTuple>();
+        WorkflowRun data = entityManager.find(WorkflowRun.class, id);
+        if (data == null){
+            return null;
+        }
         Set<Processing> affectedProcessing = new HashSet<Processing>();
         // workflow_run
         if (data.getProcessingCollection() != null) {
@@ -141,22 +163,71 @@ public class WorkflowRunFacadeREST extends AbstractFacade<WorkflowRun> {
         }
         Set<File> affectedFile = new HashSet<File>();
         for (Processing p : affectedProcessing) {
+            // look for child workflow runs and handle them recursively
+            if (p.getProcessingRelationshipCollection() != null){
+                for(ProcessingRelationship childpr : p.getProcessingRelationshipCollection()){
+                    Processing childp = childpr.getChildId();
+                    // if the child processing is connected to a workflow run that is not this one, recursive
+                    WorkflowRun childRun = childp.getAncestorWorkflowRunId();
+                    if (childRun == null){ childRun = childp.getWorkflowRunId(); }
+                    if (!childRun.equals(data)){
+                        Set<ModelAccessionIDTuple> recursiveSet = this.deleteWorkflowRunRecursive(childRun.getWorkflowRunId(), delete, matchSet);
+                        if (recursiveSet != null) {
+                            results.addAll(recursiveSet);
+                        }
+                    }
+                }
+                handleProcessingRelationshipCollection(p.getProcessingRelationshipCollection(), results); 
+                handleProcessingRelationshipCollection(p.getProcessingRelationshipCollection1(), results);          
+            }
+            
             Collection<ProcessingFiles> processingFilesCollection = p.getProcessingFilesCollection();
             if (processingFilesCollection == null) {
                 continue;
             }
             for (ProcessingFiles pf : processingFilesCollection) {
                 affectedFile.add(pf.getFileId());
-            }
+            }            
         }
-        // list all affected resources
+        // list all affected resources (and delete them if required)
         for (Processing p : affectedProcessing) {
             results.add(new ModelAccessionIDTuple(p.getSwAccession(), p.getProcessingId(), p.getClass().getName()));
         }
         for (File f : affectedFile) {
             results.add(new ModelAccessionIDTuple(f.getSwAccession(), f.getFileId(), f.getClass().getName()));
         }
+        // handle the workflow run itself
+        results.add(new ModelAccessionIDTuple(data.getSwAccession(), data.getWorkflowRunId(), data.getClass().getName()));
+        // if we are doing deletion, check that everything is ok
+        try {
+            if (delete) {
+                // check that the results are a subset of the matchset
+                if (matchSet.containsAll(results)) {
+                    for (ModelAccessionIDTuple t : results) {
+                        Object found = entityManager.find(Class.forName(t.getAdminModelClass()), t.getId());
+                        // entity may already have been removed by recursive call
+                        if (found != null){
+                            entityManager.remove(found);
+                        }
+                    }
+                } else{
+                    results.removeAll(matchSet);
+                    throw new RuntimeException("Found " + results.size() + " elements that were not in the match set");
+                }
+            }
+        } catch (ClassNotFoundException ex) {
+            throw new RuntimeException("ClassNotFound", ex);
+        }
         return results;
+    }
+
+    private void handleProcessingRelationshipCollection(Collection<ProcessingRelationship> col, Set<ModelAccessionIDTuple> results) {
+        // add the actual relationships themselves to the results
+        if (col != null){
+            for(ProcessingRelationship pr : col){
+                results.add(new ModelAccessionIDTuple(-1, pr.getProcessingRelationshipId(), pr.getClass().getName()));
+            }
+        }
     }
   
 }
