bbc8d4ff5ebc654080f0ea6ead4222e6eecd2fd9
Denis Yuen <denis.yuen@oicr.on.ca>
2013-10-24 15:51:04 -0400

    Remove dependency on seqware-common and it's spring context.
    Moved status enum values to seqware-meta-db and updated glassfish documentation
    
    
    Former-commit-id: 46786ab109f6a288a4534f8c02e6aa5e0cf8888c

--- seqware-common/src/main/java/net/sourceforge/seqware/common/util/workflowtools/WorkflowManager.java
+++ seqware-common/src/main/java/net/sourceforge/seqware/common/util/workflowtools/WorkflowManager.java
@@ -16,6 +16,7 @@
  */
 package net.sourceforge.seqware.common.util.workflowtools;
 
+import net.sourceforge.seqware.common.model.WorkflowRunStatus;
 import net.sourceforge.seqware.common.util.workflowtools.WorkflowInfo;
 
 import java.sql.SQLException;
