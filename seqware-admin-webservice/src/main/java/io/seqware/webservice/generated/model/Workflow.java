42c5dbda394988dbd775566559aa5d2f09847eb1
Denis Yuen <denis.yuen@oicr.on.ca>
2013-10-28 18:28:48 -0400

    Add in support for optimistic locking and hook deletion into it
    Unfortunately, we have to edit to actual generated classes here. Sigh.
    
    
    Former-commit-id: 61d968c38997022f158cbb8bba5bb1c766b5b643

--- seqware-admin-webservice/src/main/java/io/seqware/webservice/generated/model/Workflow.java
+++ seqware-admin-webservice/src/main/java/io/seqware/webservice/generated/model/Workflow.java
@@ -5,6 +5,7 @@
 package io.seqware.webservice.generated.model;
 
 import java.io.Serializable;
+import java.sql.Timestamp;
 import java.util.Collection;
 import java.util.Date;
 import javax.persistence.Basic;
@@ -22,6 +23,7 @@ import javax.persistence.OneToMany;
 import javax.persistence.Table;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
+import javax.persistence.Version;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 import javax.xml.bind.annotation.XmlRootElement;
@@ -102,8 +104,8 @@ public class Workflow implements Serializable {
   @Temporal(TemporalType.TIMESTAMP)
   private Date createTstmp;
   @Column(name = "update_tstmp")
-  @Temporal(TemporalType.TIMESTAMP)
-  private Date updateTstmp;
+  @Version
+  private Timestamp updateTstmp;
   @Column(name = "sw_accession")
   private Integer swAccession;
   @Size(max = 2147483647)
@@ -244,11 +246,11 @@ public class Workflow implements Serializable {
     this.createTstmp = createTstmp;
   }
 
-  public Date getUpdateTstmp() {
+  public Timestamp getUpdateTstmp() {
     return updateTstmp;
   }
 
-  public void setUpdateTstmp(Date updateTstmp) {
+  public void setUpdateTstmp(Timestamp updateTstmp) {
     this.updateTstmp = updateTstmp;
   }
 
