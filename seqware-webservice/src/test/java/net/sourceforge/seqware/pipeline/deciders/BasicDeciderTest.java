61c2922cec4f4ce14c9eff233e284d3b46893d78
Denis Yuen <denis.yuen@oicr.on.ca>
2013-10-31 15:37:09 -0400

    Add filemetadata test

--- seqware-webservice/src/test/java/net/sourceforge/seqware/pipeline/deciders/BasicDeciderTest.java
+++ seqware-webservice/src/test/java/net/sourceforge/seqware/pipeline/deciders/BasicDeciderTest.java
@@ -16,6 +16,7 @@
  */
 package net.sourceforge.seqware.pipeline.deciders;
 
+import io.seqware.Reports;
 import java.io.ByteArrayOutputStream;
 import java.io.PrintStream;
 import java.util.ArrayList;
@@ -24,6 +25,8 @@ import java.util.List;
 import java.util.Map;
 import java.util.Set;
 import java.util.TreeMap;
+import net.sourceforge.seqware.common.hibernate.FindAllTheFiles;
+import net.sourceforge.seqware.common.hibernate.FindAllTheFiles.Header;
 import net.sourceforge.seqware.common.metadata.MetadataWS;
 import net.sourceforge.seqware.common.module.FileMetadata;
 import net.sourceforge.seqware.common.module.ReturnValue;
@@ -48,6 +51,7 @@ public class BasicDeciderTest extends PluginTest {
     @BeforeClass
     public static void beforeClass(){
         BasicTestDatabaseCreator.resetDatabaseWithUsers();
+        Reports.triggerProvenanceReport();
     }
 
     @Before
@@ -204,14 +208,13 @@ public class BasicDeciderTest extends PluginTest {
     
     @Test
     public void testSEQWARE1297DoNotLaunchFailedWorkflows() {
-        TestingDecider decider = (TestingDecider) instance;
         // trying to find a good test for this, doesn't look like there is one in the testing database?
         // there is one pending workflow_run as revealed by "select * from workflow_run  WHERE status = 'pending';"
         // however, it doesn't appear to be properly linked in 
         // see "select sh.*, s.* FROM sample_hierarchy sh , (select DISTINCT s.sample_id from workflow_run wr, ius_workflow_runs iwr, ius, sample s WHERE status = 'pending' AND wr.workflow_run_id=iwr.workflow_run_id AND iwr.ius_id=ius.ius_id AND ius.sample_id=s.sample_id) sq, sample s WHERE sh.sample_id=sq.sample_id AND s.sample_id=sh.parent_id;"
         String[] params = new String[]{"--sample", "", "--wf-accession", "4773", "--meta-types", "application/bam,text/vcf-4,chemical/seq-na-fastq-gzip", "--rerun-max", "10", "--test"};
         launchAndCaptureOutput(params);
-        decider = (TestingDecider) instance;
+        TestingDecider decider = (TestingDecider) instance;
         Assert.assertTrue("output does not contain the correct number of files, we saw " + decider.getFileCount(), decider.getFileCount() == 68);
         Assert.assertTrue("output does not contain the correct number of launches, we saw " + decider.getLaunches(), decider.getLaunches() == 57);
         
@@ -221,21 +224,68 @@ public class BasicDeciderTest extends PluginTest {
         Assert.assertTrue("output does not contain the correct number of launches, we saw " + decider.getLaunches(), decider.getLaunches() == 55);
     }
     
+    @Test 
+    public void testDecidingWithAttributes(){
+        
+    }
+    
+    @Test
+    public void testDecidingWithFilemetadata(){
+        
+        // swap out the decider
+        instance = new FileMetadataDecider();
+        //instance = new BasicDecider();
+        instance.setMetadata(metadata);  
+        FileMetadataDecider decider = (FileMetadataDecider) instance;
+
+        
+        //update the database so targetted files will contain full metadata
+        dbCreator.runUpdate("update file set md5sum = sw_accession + 42 WHERE sw_accession IN (SELECT file_swa from file_provenance_report WHERE study_swa=120);");
+        dbCreator.runUpdate("update file set description = 'funky_description' WHERE sw_accession IN (SELECT file_swa from file_provenance_report WHERE study_swa=120);");
+        dbCreator.runUpdate("update file set size = sw_accession + 1701 WHERE sw_accession IN (SELECT file_swa from file_provenance_report WHERE study_swa=120);");
+        // use a special decider to ensure that filemetadata is populated
+        String[] params = {"--study-name", "AbcCo_Exome_Sequencing", "--wf-accession", "4", "--meta-types", "text/h-tumour,application/vcf-4-gzip,text/annovar-tags,application/zip-report-bundle,txt,chemical/seq-na-fastq-gzip,application/bam,text/vcf-4,chemical/seq-na-fastq", "--test"};
+        launchAndCaptureOutput(params);
+        Assert.assertTrue("output does not contain the correct number of files, we saw " + decider.getFileCount(), decider.getFileCount() == 21);
+        Assert.assertTrue("we didn't check the correct number of files, we checked " + decider.filesChecked, decider.filesChecked == 21);
+        
+        // swap back the decider
+        instance = new TestingDecider();
+        //instance = new BasicDecider();
+        instance.setMetadata(metadata);
+    }
+    
     public class HaltingDecider extends TestingDecider{
         boolean haltedOnce = false;
         @Override
         protected ReturnValue doFinalCheck(String commaSeparatedFilePaths, String commaSeparatedParentAccessions) {
             super.doFinalCheck(commaSeparatedFilePaths, commaSeparatedParentAccessions);
-            ReturnValue ret;
+            ReturnValue localRet;
             if (!haltedOnce){
                 haltedOnce = true;
-                ret = new ReturnValue(ReturnValue.FAILURE);
+                localRet = new ReturnValue(ReturnValue.FAILURE);
             } else{
-                ret = new ReturnValue(ReturnValue.SUCCESS);
+                localRet = new ReturnValue(ReturnValue.SUCCESS);
             }
-            return ret;
+            return localRet;
         }
         
+    }
+    
+    public class FileMetadataDecider extends TestingDecider{
+        int filesChecked = 0;
+         @Override
+        protected boolean checkFileDetails(ReturnValue returnValue, FileMetadata fm) {
+             filesChecked++;
+             int file_swa = Integer.valueOf(returnValue.getAttribute(Header.FILE_SWA.getTitle()));
+             Assert.assertTrue("file path is empty " + file_swa, !(fm.getFilePath() == null) && !fm.getFilePath().isEmpty());
+             // FindAllTheFiles doesn't actually populate this information, go figure
+             //Assert.assertTrue("file md5sum is wrong for " + file_swa, fm.getMd5sum().equals(Integer.toString(file_swa+42)));
+             //Assert.assertTrue("file size is wrong", fm.getSize().equals((long)file_swa+1701));
+             Assert.assertTrue("file description is empty" + file_swa, !(fm.getDescription()== null) && !fm.getDescription().isEmpty());
+             Assert.assertTrue("meta type is empty" + file_swa , !(fm.getMetaType()== null) && !fm.getMetaType().isEmpty());
+             return false;
+        }
     }
 
     public class TestingDecider extends BasicDecider {
