3c8ea647389c5adfa1c5d7a8190b7f66e62edf50
Tony DeBat <tony.debat@gmail.com>
2013-11-06 15:59:42 -0500

    Added resources to allow an attribute to be created at the same time as an entity.
    Former-commit-id: a006cf76a724ab611a02be77c901470a53a752da

--- seqware-admin-webservice/src/main/java/io/seqware/webservice/controller/CustomSampleAttributeFacadeREST.java
+++ seqware-admin-webservice/src/main/java/io/seqware/webservice/controller/CustomSampleAttributeFacadeREST.java
@@ -4,15 +4,50 @@
  */
 package io.seqware.webservice.controller;
 
-import io.seqware.webservice.generated.controller.*;
+import io.seqware.webservice.generated.controller.SampleAttributeFacadeREST;
+import io.seqware.webservice.generated.controller.SampleFacadeREST;
+import io.seqware.webservice.generated.model.SampleAttribute;
+
+import javax.ejb.EJB;
 import javax.ejb.Stateless;
+import javax.ejb.TransactionAttribute;
+import javax.ejb.TransactionAttributeType;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 
 /**
- *
+ * 
  * @author dyuen
  */
 @Stateless
 @Path("io.seqware.webservice.model.sampleattribute")
 public class CustomSampleAttributeFacadeREST extends SampleAttributeFacadeREST {
+
+   @PersistenceContext(unitName = "io.seqware_seqware-admin-webservice_war_1.0-SNAPSHOTPU")
+   private EntityManager em;
+
+   @EJB
+   private SampleFacadeREST sampleFacadeRest;
+
+   /**
+    * Create a new sample and a new sample attribute to associate with it in a
+    * single step. Both steps occur in a single transaction.
+    * 
+    * @param entity
+    *           The sample attribute with nested sample.
+    */
+   @TransactionAttribute(TransactionAttributeType.REQUIRES_NEW)
+   @POST
+   @Path("withsample")
+   @Consumes({ "application/json", "application/xml" })
+   public void createWithSample(SampleAttribute entity) {
+      System.out.println("Study id should be null " + entity.getSampleId().getSampleId());
+      sampleFacadeRest.create(entity.getSampleId());
+      em.flush();
+      System.out.println("Study id should have value " + entity.getSampleId().getSampleId());
+      super.create(entity);
+   }
 }
--- seqware-admin-webservice/src/main/java/io/seqware/webservice/controller/CustomSequencerRunAttributeFacadeREST.java
+++ seqware-admin-webservice/src/main/java/io/seqware/webservice/controller/CustomSequencerRunAttributeFacadeREST.java
@@ -4,15 +4,50 @@
  */
 package io.seqware.webservice.controller;
 
-import io.seqware.webservice.generated.controller.*;
+import io.seqware.webservice.generated.controller.SequencerRunAttributeFacadeREST;
+import io.seqware.webservice.generated.controller.SequencerRunFacadeREST;
+import io.seqware.webservice.generated.model.SequencerRunAttribute;
+
+import javax.ejb.EJB;
 import javax.ejb.Stateless;
+import javax.ejb.TransactionAttribute;
+import javax.ejb.TransactionAttributeType;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 
 /**
- *
+ * 
  * @author dyuen
  */
 @Stateless
 @Path("io.seqware.webservice.model.sequencerrunattribute")
 public class CustomSequencerRunAttributeFacadeREST extends SequencerRunAttributeFacadeREST {
+
+   @PersistenceContext(unitName = "io.seqware_seqware-admin-webservice_war_1.0-SNAPSHOTPU")
+   private EntityManager em;
+
+   @EJB
+   private SequencerRunFacadeREST sequencerRunFacadeRest;
+
+   /**
+    * Create a new sequencer run and a new sequencer run attribute to associate
+    * with it in a single step. Both steps occur in a single transaction.
+    * 
+    * @param entity
+    *           The sequencer run attribute with nested sequencer run.
+    */
+   @TransactionAttribute(TransactionAttributeType.REQUIRES_NEW)
+   @POST
+   @Path("withsequencerrun")
+   @Consumes({ "application/json", "application/xml" })
+   public void createWithSample(SequencerRunAttribute entity) {
+      System.out.println("Study id should be null " + entity.getSampleId().getSequencerRunId());
+      sequencerRunFacadeRest.create(entity.getSampleId());
+      em.flush();
+      System.out.println("Study id should have value " + entity.getSampleId().getSequencerRunId());
+      super.create(entity);
+   }
 }
