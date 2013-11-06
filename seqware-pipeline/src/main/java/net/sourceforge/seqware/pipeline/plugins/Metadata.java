051ea6c17d1727ca10b17ff83cb4f16d318e0c84
Denis Yuen <denis.yuen@oicr.on.ca>
2013-11-06 11:49:08 -0500

    Remove trigger language
    Former-commit-id: cffa8a705188f6409c3d3fee5725961bfeb8cbae

--- seqware-pipeline/src/main/java/net/sourceforge/seqware/pipeline/plugins/Metadata.java
+++ seqware-pipeline/src/main/java/net/sourceforge/seqware/pipeline/plugins/Metadata.java
@@ -751,7 +751,7 @@ public class Metadata extends Plugin {
     public String get_description() {
         return "This plugin lets you list, read, and write to a collection of tables in the underlying MetaDB. "
                 + "This makes it easier to automate the creation of entities in the database which can be used as "
-                + "parents for file uploads and triggered workflows.";
+                + "parents for file uploads and launched workflows.";
     }
 
     protected void closeBufferWriter() {
