2d393d0f2fdd871fb2a4c5db9cb7a26ece0c4ed6
Denis Yuen <denis.yuen@oicr.on.ca>
2013-11-22 15:53:56 -0500

    Initial prototype for the tool including a check that runs through the pre-CLI tutorial
    We need to break the dependency on mvn archetype:generate as well as the use of /tmp which is not guaranteed to work across clusters
    
    
    Former-commit-id: fe161f4616f7c37ff2f56d44a9e59f917a79c8e8

---
+++ seqware-sanity-check/src/main/java/io/seqware/pipeline/plugins/sanity/SanityCheckPluginInterface.java
@@ -0,0 +1,35 @@
+/*
+ * Copyright (C) 2013 SeqWare
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package io.seqware.pipeline.plugins.sanity;
+
+import java.sql.SQLException;
+import net.sourceforge.seqware.common.metadata.Metadata;
+
+/**
+ * Specifies an interface for checks that can be done for a SeqWare install
+ * @author dyuen
+ */
+public interface SanityCheckPluginInterface {
+    /**
+     * 
+     * @param qRunner a runner constructed using your database options, may not be present for non-admins
+     * @param metadataWS a metadata interface constructed using the web service interface
+     * @return whether the check succeeded
+     * @throws SQLException 
+     */
+    public boolean check(QueryRunner qRunner, Metadata metadataWS) throws SQLException;
+}
