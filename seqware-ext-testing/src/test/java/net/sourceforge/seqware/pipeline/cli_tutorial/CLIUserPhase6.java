9824536840b1fc9b2f5b16a588d3554547c54915
Denis Yuen <denis.yuen@oicr.on.ca>
2013-12-06 17:41:53 -0500

    Fix extended test

--- seqware-ext-testing/src/test/java/net/sourceforge/seqware/pipeline/cli_tutorial/CLIUserPhase6.java
+++ seqware-ext-testing/src/test/java/net/sourceforge/seqware/pipeline/cli_tutorial/CLIUserPhase6.java
@@ -49,7 +49,7 @@ public class CLIUserPhase6 extends UserPhase6{
 
     @Override
     protected void exportStudyResults() throws IOException {
-        ITUtility.runSeqwareCLI(" files report --study 'New Test Study'", ReturnValue.SUCCESS, null);
+        ITUtility.runSeqwareCLI(" files report --study-name 'New Test Study'", ReturnValue.SUCCESS, null);
     }
 
 }
