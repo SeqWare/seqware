c28c2603545d6b1cf9eee3cbe751ef7fd8a454c9
Denis Yuen <dyuen@oicr.on.ca>
2014-02-04 14:57:52 -0500

    Prototype for naive batching
    Former-commit-id: 17083530a199fe297a74ebebde64d5102aeea983

---
+++ seqware-pipeline/src/main/java/net/sourceforge/seqware/pipeline/workflowV2/model/JobBatch.java
@@ -0,0 +1,89 @@
+package net.sourceforge.seqware.pipeline.workflowV2.model;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+
+public class JobBatch extends AbstractJob {
+
+    private List<Job> jobList = new ArrayList<Job>();
+
+    public JobBatch(String algo) {
+        super(algo);
+    }
+
+    public Job createBashJob(String algo) {
+        AbstractJob job = new BashJob(algo);
+        this.jobList.add(job);
+        return job;
+    }
+
+    @Override
+    public Command setCommand(String cmd) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public Command getCommand() {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public AbstractJob setMaxMemory(String mem) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public String getMaxMemory() {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public AbstractJob setThreads(int count) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public int getThreads() {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public Job setQueue(String queue) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public void setHasMetadataWriteback(boolean metadata) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public boolean hasMetadataWriteback() {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public void setParentAccessions(Collection<String> parentAccessions) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public boolean isLocal() {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public void setLocal() {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public void setLocal(boolean runLocal) {
+        throw new UnsupportedOperationException();
+    }
+
+    public List<Job> getJobList() {
+        return jobList;
+    }
+}
