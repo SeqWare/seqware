aa1aac1853958607a719d5b166ba5345e7cea242
RaunaqSuri <raunaq.suri@hotmail.com>
2014-03-05 15:06:18 -0500

    more formatting

--- seqware-pipeline/src/main/java/net/sourceforge/seqware/pipeline/plugins/batchmetadatainjection/RunInfo.java
+++ seqware-pipeline/src/main/java/net/sourceforge/seqware/pipeline/plugins/batchmetadatainjection/RunInfo.java
@@ -516,7 +516,7 @@ public class RunInfo {
         for (LaneInfo lane: lanes) {
             lane.print(writer, metadata);
         }
-        writer.append("}");
+        writer.append("\n\t}\n}");
     }
     
     
