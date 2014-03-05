18c31571c2a25ce06613c26f25cede36d2789e28
Michael Laszloffy <michael.laszloffy@oicr.on.ca>
2014-03-05 17:14:57 -0500

    Import and export BatchMetadataInjection RunInfo, LaneInfo, and SampleInfo objects as json

--- seqware-pipeline/src/main/java/net/sourceforge/seqware/pipeline/plugins/batchmetadatainjection/LaneInfo.java
+++ seqware-pipeline/src/main/java/net/sourceforge/seqware/pipeline/plugins/batchmetadatainjection/LaneInfo.java
@@ -21,7 +21,6 @@ import java.io.IOException;
 import java.util.HashSet;
 import java.util.Set;
 import net.sourceforge.seqware.common.metadata.Metadata;
-import net.sourceforge.seqware.common.model.LaneAttribute;
 import net.sourceforge.seqware.common.model.StudyType;
 import org.apache.commons.lang.StringUtils;
 
@@ -41,7 +40,7 @@ public class LaneInfo {
     private String librarySelectionAcc = null;
     private String studyTypeAcc = null;
     private Set<SampleInfo> samples;
-    private Set<LaneAttribute> laneAttributes;
+    private Set<TagValueUnit> laneAttributes;
 
     /**
      * Get the value of studyTypeAcc
@@ -61,9 +60,9 @@ public class LaneInfo {
         this.studyTypeAcc = String.valueOf(studyTypeAcc);
     }
 
-    public Set<LaneAttribute> getLaneAttributes() {
+    public Set<TagValueUnit> getLaneAttributes() {
         if (laneAttributes == null) {
-            laneAttributes = new HashSet<LaneAttribute>();
+            laneAttributes = new HashSet<TagValueUnit>();
         }
         return laneAttributes;
     }
@@ -77,8 +76,8 @@ public class LaneInfo {
      */
     public void setLaneAttribute(String tag, String value) {
 
-        LaneAttribute sa = null;
-        for (LaneAttribute s : getLaneAttributes()) {
+        TagValueUnit sa = null;
+        for (TagValueUnit s : getLaneAttributes()) {
             if (s.getTag().equals(tag.trim())) {
                 sa = s;
                 break;
@@ -90,14 +89,14 @@ public class LaneInfo {
             return;
         }
         if (sa == null) {
-            sa = new LaneAttribute();
+            sa = new TagValueUnit();
             getLaneAttributes().add(sa);
         }
         sa.setTag(tag.trim());
         sa.setValue(value.trim());
     }
 
-    public void setLaneAttributes(Set<LaneAttribute> laneAttributes) {
+    public void setLaneAttributes(Set<TagValueUnit> laneAttributes) {
         this.laneAttributes = laneAttributes;
     }
 
