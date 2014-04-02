282f4a3bd00041f1635126fe9ad78e30249b0209
Denis Yuen <dyuen@oicr.on.ca>
2014-04-02 10:09:22 -0400

    Remove testng and convert json tests to junit
    Former-commit-id: c2374c494c39c103e9321fc492897e5c3085a884

--- seqware-common/src/test/java/net/sourceforge/seqware/common/util/jsontools/JSONHelperTest.java
+++ seqware-common/src/test/java/net/sourceforge/seqware/common/util/jsontools/JSONHelperTest.java
@@ -1,8 +1,8 @@
 package net.sourceforge.seqware.common.util.jsontools;
 
 import java.io.InputStream;
-import org.testng.Assert;
-import org.testng.annotations.Test;
+import org.junit.Assert;
+import org.junit.Test;
 
 /**
  * Unit tests
@@ -16,36 +16,36 @@ public class JSONHelperTest {
     /**
      * Checks to ensure that all required parameters are met
      */
-    @Test(testName = "Missing required JSON parameter")
+    @Test
     public void missingRequired() {
         InputStream schema = this.getClass().getClassLoader().getResourceAsStream("schema.json");
         InputStream testData = this.getClass().getClassLoader().getResourceAsStream("missingField.json");
         Assert.assertNotNull(testData);
-        Assert.assertFalse(jman.isJSONValid(schema, testData), "ERROR: JSON is valid, when it should be invalid");
+        Assert.assertFalse("ERROR: JSON is valid, when it should be invalid", jman.isJSONValid(schema, testData));
     }
 
     /**
      * Checks to make sure that there are configurations for the tests in the
      * JSON
      */
-    @Test(testName = "No Test Config")
+    @Test
     public void noTestConfig() {
         InputStream schema = this.getClass().getClassLoader().getResourceAsStream("schema.json");
         InputStream testData = this.getClass().getClassLoader().getResourceAsStream("noTestConfig.json");
         Assert.assertNotNull(testData);
-        Assert.assertFalse(jman.isJSONValid(schema, testData), "ERROR: JSON is valid when it should be invalid");
+        Assert.assertFalse("ERROR: JSON is valid when it should be invalid", jman.isJSONValid(schema, testData));
     }
 
     /**
      * Checks to make sure that a null pointer exception is thrown if the files
      * don't exist
      */
-    @Test(testName = "Bad Resource Names", expectedExceptions = NullPointerException.class)
+    @Test(expected = NullPointerException.class)
     public void badResourceNames() {
         InputStream schema = this.getClass().getClassLoader().getResourceAsStream("schema.json");
         InputStream data = this.getClass().getClassLoader().getResourceAsStream("data.json");
-        Assert.assertNull(jman.isJSONValid(this.getClass().getClassLoader().getResourceAsStream("Raunaq.json"), data), "ERROR: Schema should be null as the file doesn't exist");
-        Assert.assertNull(jman.isJSONValid(schema, this.getClass().getClassLoader().getResourceAsStream("Suri.json")), "ERROR: Data should be null as the file doesn't exist");
+        Assert.assertNull("ERROR: Schema should be null as the file doesn't exist", jman.isJSONValid(this.getClass().getClassLoader().getResourceAsStream("Raunaq.json"), data));
+        Assert.assertNull("ERROR: Data should be null as the file doesn't exist", jman.isJSONValid(schema, this.getClass().getClassLoader().getResourceAsStream("Suri.json")));
 
     }
 
@@ -53,7 +53,7 @@ public class JSONHelperTest {
      * Checks to make sure that if the environment data is not written properly,
      * a false is returned
      */
-    @Test(testName = "Environment Data Malformed")
+    @Test
     public void environmentDataMalformed() {
         InputStream schema = this.getClass().getClassLoader().getResourceAsStream("schema.json");
         InputStream testData = this.getClass().getClassLoader().getResourceAsStream("environmentMalformed.json");
@@ -64,10 +64,10 @@ public class JSONHelperTest {
     /**
      * The data perfectly matches the schema
      */
-    @Test(testName = "Everything is Perfect")
+    @Test
     public void everythingIsPerfect() {
         InputStream schema = this.getClass().getClassLoader().getResourceAsStream("schema.json");
         InputStream data = this.getClass().getClassLoader().getResourceAsStream("data.json");
-        Assert.assertTrue(jman.isJSONValid(schema, data), "ERROR: Perfect match gave false instead of true");
+        Assert.assertTrue("ERROR: Perfect match gave false instead of true", jman.isJSONValid(schema, data));
     }
 }
