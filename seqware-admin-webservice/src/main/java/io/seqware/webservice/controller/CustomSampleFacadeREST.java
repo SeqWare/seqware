bf513c9d367672d0fba5181c7132d966621e2892
Denis Yuen <dyuen@oicr.on.ca>
2014-06-13 14:09:47 -0400

    Sample implementation of sample hierarchy strategy
    A lifecycle callback allows for the population of null relationships to
    a parent at load time. A new web resource demonstrates how to create new
    null relationships.

--- seqware-admin-webservice/src/main/java/io/seqware/webservice/controller/CustomSampleFacadeREST.java
+++ seqware-admin-webservice/src/main/java/io/seqware/webservice/controller/CustomSampleFacadeREST.java
@@ -4,9 +4,19 @@
  */
 package io.seqware.webservice.controller;
 
+import com.sun.jersey.api.ConflictException;
+import com.sun.jersey.api.NotFoundException;
 import io.seqware.webservice.generated.controller.*;
+import io.seqware.webservice.generated.model.Sample;
+import java.util.Set;
 import javax.ejb.Stateless;
+import javax.ejb.TransactionAttribute;
+import javax.ejb.TransactionAttributeType;
+import javax.persistence.Query;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 
 /**
  *
@@ -15,4 +25,31 @@ import javax.ws.rs.Path;
 @Stateless
 @Path("io.seqware.webservice.model.sample")
 public class CustomSampleFacadeREST extends SampleFacadeREST {
+    
+    /**
+     * Modify the listed sample to create a sample_hierarchy relation with a
+     * null parent
+     *
+     * @param id
+     * @return 
+     */
+    @TransactionAttribute(TransactionAttributeType.REQUIRES_NEW)
+    @PUT
+    @Path("{id}/createNullHierarchy")
+    @Consumes({"application/xml", "application/json"})
+    public void createNullHierarchy(@PathParam("id") Integer id) {
+        // verify that the entity exists
+        Sample find = super.find(id);
+        // verify that no null relation already exists
+        Query createSelectNullParentCountQuery = Sample.createSelectNullParentCountQuery(super.getEntityManager(), id);
+        Long count = (Long)createSelectNullParentCountQuery.getSingleResult();
+        // create a null relationship
+        if (count == 0){
+            int executeUpdate = super.getEntityManager().createNativeQuery("insert into sample_hierarchy values ("+id+", null);").executeUpdate();
+            if (executeUpdate == 1){
+                return;
+            }
+            throw new ConflictException("could not create null parent hierarchy relationship");
+        } 
+    }
 }
