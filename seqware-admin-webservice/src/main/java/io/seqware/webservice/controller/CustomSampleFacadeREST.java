e3277a36884ff4f414f7f5fc1c543ef055f790c7
mnerandzic <Marko.Nerandzic@oicr.on.ca>
2014-06-18 14:01:00 -0400

    Added a method to delete rows from the sample_hierarchy table with a null parent_id and renamed createSelectNullParentCountQuery method to selectNullParentCountQuery
    Former-commit-id: 63206bc0a78b999eaa87b62c36172ab6030cbdf1

--- seqware-admin-webservice/src/main/java/io/seqware/webservice/controller/CustomSampleFacadeREST.java
+++ seqware-admin-webservice/src/main/java/io/seqware/webservice/controller/CustomSampleFacadeREST.java
@@ -4,22 +4,23 @@
  */
 package io.seqware.webservice.controller;
 
-import com.sun.jersey.api.ConflictException;
-import com.sun.jersey.api.NotFoundException;
-import io.seqware.webservice.generated.controller.*;
+import io.seqware.webservice.generated.controller.SampleFacadeREST;
 import io.seqware.webservice.generated.model.Sample;
-import java.util.Set;
+
 import javax.ejb.Stateless;
 import javax.ejb.TransactionAttribute;
 import javax.ejb.TransactionAttributeType;
 import javax.persistence.Query;
 import javax.ws.rs.Consumes;
+import javax.ws.rs.DELETE;
 import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 
+import com.sun.jersey.api.ConflictException;
+
 /**
- *
+ * 
  * @author dyuen
  */
 @Stateless
@@ -41,7 +42,7 @@ public class CustomSampleFacadeREST extends SampleFacadeREST {
         // verify that the entity exists
         Sample find = super.find(id);
         // verify that no null relation already exists
-        Query createSelectNullParentCountQuery = Sample.createSelectNullParentCountQuery(super.getEntityManager(), id);
+        Query createSelectNullParentCountQuery = Sample.selectNullParentCountQuery(super.getEntityManager(), id);
         Long count = (Long)createSelectNullParentCountQuery.getSingleResult();
         // create a null relationship
         if (count == 0){
@@ -50,6 +51,28 @@ public class CustomSampleFacadeREST extends SampleFacadeREST {
                 return;
             }
             throw new ConflictException("could not create null parent hierarchy relationship");
-        } 
+        }
+    }
+
+    @TransactionAttribute(TransactionAttributeType.REQUIRED)
+    @DELETE
+    @Path("{id}/removeNullHierarchy")
+    @Consumes({ "application/xml", "application/json" })
+    public void removeNullHierarchy(@PathParam("id") Integer id) {
+        // verify that the entity exists
+        Sample find = super.find(id);
+        // verify that a null relation already exists
+        Query removeSelectNullParentCountQuery = Sample.selectNullParentCountQuery(super.getEntityManager(), id);
+        Long count = (Long) removeSelectNullParentCountQuery.getSingleResult();
+        // remove the null relationship
+        if (count == 1) {
+            int executeUpdate = super.getEntityManager()
+                    .createNativeQuery("DELETE FROM sample_hierarchy sh WHERE sh.sample_id=" + id + " AND sh.parent_id IS NULL;")
+                    .executeUpdate();
+            if (executeUpdate == 1) {
+                return;
+            }
+            throw new ConflictException("could not remove null parent hierarchy relationship");
+        }
     }
 }
