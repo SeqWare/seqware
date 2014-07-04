36ea50f260f487e3f343fbec5b5257eae1f992aa
Denis Yuen <dyuen@oicr.on.ca>
2014-07-04 18:06:13 -0400

    Finish removing the link tables and remove the sample search table as well
    Former-commit-id: a3a8b44d630acb4d6b0a312b1eeb06887c8beb77

--- seqware-meta-db/1.0.x_to_1.1.0.sql
+++ seqware-meta-db/1.0.x_to_1.1.0.sql
@@ -3,3 +3,11 @@ drop table share_file;
 drop table share_lane;
 drop table share_sample;
 drop table share_processing;
+drop table experiment_link;
+drop table sample_link;
+drop table study_link;
+drop table ius_link;
+drop table lane_link;
+drop table sample_search;
+drop table sample_search_attribute;
+
