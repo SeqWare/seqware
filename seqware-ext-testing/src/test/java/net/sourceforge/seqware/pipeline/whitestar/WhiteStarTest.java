8435a7eda91883056e450068c5212600f1bc19d3
Denis Yuen <dyuen@oicr.on.ca>
2014-10-29 18:18:54 -0400

    Add evil way of calling whitestar to allow code coverage to do its thing

--- seqware-ext-testing/src/test/java/net/sourceforge/seqware/pipeline/whitestar/WhiteStarTest.java
+++ seqware-ext-testing/src/test/java/net/sourceforge/seqware/pipeline/whitestar/WhiteStarTest.java
@@ -16,11 +16,13 @@
  */
 package net.sourceforge.seqware.pipeline.whitestar;
 
+import io.seqware.cli.Main;
 import java.io.File;
-import java.io.IOException;
+import java.lang.reflect.Field;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 import java.util.Map;
 import net.sourceforge.seqware.common.module.ReturnValue;
@@ -40,7 +42,7 @@ import org.junit.Test;
 public class WhiteStarTest {
 
     @Test
-    public void testWorkflow() throws IOException {
+    public void testWorkflow() throws Exception {
         // override seqware settings file
         List<String> whiteStarProperties = new ArrayList<>();
         whiteStarProperties.add("SW_METADATA_METHOD=inmemory");
@@ -81,7 +83,37 @@ public class WhiteStarTest {
         environment.put("SEQWARE_SETTINGS", createTempFile.toAbsolutePath().toString());
         String runSeqwareCLI = ITUtility.runSeqwareCLI(" bundle launch  --no-metadata --dir " + bundleDir.getAbsolutePath(),
                 ReturnValue.SUCCESS, tempDir.toFile(), environment);
+
+        // save system environment variables
+        Map<String, String> env = System.getenv();
+
+        // override for launching
+        WhiteStarTest.set(environment);
+        Main.main(new String[] { "bundle", "launch", "--no-metadata", "--dir", bundleDir.getAbsolutePath() });
+        WhiteStarTest.set(env);
+
         System.out.println(runSeqwareCLI);
+    }
+
+    /**
+     * Evil https://stackoverflow.com/questions/318239/how-do-i-set-environment-variables-from-java
+     * 
+     * @param newenv
+     * @throws Exception
+     */
+    public static void set(Map<String, String> newenv) throws Exception {
+        Class[] classes = Collections.class.getDeclaredClasses();
+        Map<String, String> env = System.getenv();
+        for (Class cl : classes) {
+            if ("java.util.Collections$UnmodifiableMap".equals(cl.getName())) {
+                Field field = cl.getDeclaredField("m");
+                field.setAccessible(true);
+                Object obj = field.get(env);
+                Map<String, String> map = (Map<String, String>) obj;
+                map.clear();
+                map.putAll(newenv);
+            }
+        }
     }
 
 }
